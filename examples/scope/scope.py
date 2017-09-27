88b03cec5264b0b693e39e67c0efef5dc1004288
Gerard Marull-Paretas <gmarull@ingeniamc.com>
2017-09-27 14:55:59 +0200

    examples: scope: refactor (use state machine, thread+worker pattern, etc.)

--- examples/scope/scope.py
+++ examples/scope/scope.py
@@ -1,16 +1,17 @@
 import sys
 from os.path import join, dirname, abspath
 
-from PyQt5 import uic
-from qtpy.QtCore import Qt, QTimer, Signal, Slot, QThread
+from qtpy import uic
+from qtpy.QtCore import Qt, QObject, QTimer, Signal, Slot, QThread
 from qtpy.QtGui import (QStandardItemModel, QStandardItem, QImage, QPixmap,
                         QPalette)
-from qtpy.QtWidgets import QApplication
-import qtawesome as qta
+from qtpy.QtWidgets import QApplication, QMainWindow
 
-import numpy as np
+import qtawesome as qta
 import qtmodern.styles
 import qtmodern.windows
+
+import numpy as np
 import ingenialink as il
 
 
@@ -24,37 +25,28 @@ VEL_ACTUAL = il.Register(0x606C, 0x00, il.DTYPE_S32, il.ACCESS_RW, il.PHY_VEL)
 """ Register: actual velocity. """
 
 
-# load scope UI
-Ui_ScopeWindow, QtBaseClass = uic.loadUiType(join(_RESOURCES, 'ui',
-                                                  'scopewindow.ui'))
-
-
-class HomingRunner(QThread):
-    """ Asynchronous homing runner. """
-
-    _HOMING_TIMEOUT = 150000
-    """ int: Default homing timeout (ms). """
+class HomingRunner(QObject):
+    """ Homing runner. """
 
     finished = Signal(str)
     """ Signal: Finished signal. """
 
-    def __init__(self, axis):
-        QThread.__init__(self)
+    def __init__(self, axis, timeout):
+        QObject.__init__(self)
 
         self._axis = axis
+        self._timeout = timeout
 
     def run(self):
-        # enable in homing mode
         try:
             self._axis.mode = il.MODE_HOMING
             self._axis.enable()
         except Exception as exc:
             self.finished.emit('Error: ' + str(exc))
 
-        # run homing
         try:
             self._axis.homing_start()
-            self._axis.homing_wait(self._HOMING_TIMEOUT)
+            self._axis.homing_wait(self._timeout)
 
             self.finished.emit('Finished')
         except Exception as exc:
@@ -63,7 +55,7 @@ class HomingRunner(QThread):
             self._axis.disable()
 
 
-class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
+class ScopeWindow(QMainWindow):
     """ Scope Window. """
 
     _AXIS_TIMEOUT = 100
@@ -87,60 +79,27 @@ class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
     _VRANGE = 40
     """ int: Velocity range (+/-) rps. """
 
+    _HOMING_TIMEOUT = 150000
+    """ int: Default homing timeout (ms). """
+
+    stateInit, stateIdle, stateHoming, statePosition, stateVelocity = range(5)
+    """ States. """
+
     tabHomingIndex, tabPositionIndex, tabVelocityIndex = range(3)
     """ Motion control tabs. """
 
     def __init__(self):
-        QtBaseClass.__init__(self)
-        Ui_ScopeWindow.__init__(self)
+        QMainWindow.__init__(self)
 
-        self.setupUi(self)
-
-        self.splitter.setStretchFactor(0, 1)
-        self.splitter.setStretchFactor(1, 0)
-
-        self.tabsMotionControl.setCurrentIndex(self.tabHomingIndex)
-
-        palette = QApplication.instance().palette()
-        iconsColor = palette.color(QPalette.Text)
-
-        self.tabsMotionControl.setTabIcon(
-                self.tabHomingIndex, qta.icon('fa.home', color=iconsColor))
-        self.tabsMotionControl.setTabIcon(
-                self.tabPositionIndex, qta.icon('fa.arrows', color=iconsColor))
-        self.tabsMotionControl.setTabIcon(
-                self.tabVelocityIndex,
-                qta.icon('fa.tachometer', color=iconsColor))
-
-        self.dialPosition.setMinimum(-self._PRANGE)
-        self.dialPosition.setMaximum(self._PRANGE)
-        self.dialPosition.setValue(0)
-
-        self.dialVelocity.setMinimum(-self._VRANGE)
-        self.dialVelocity.setMaximum(self._VRANGE)
-        self.dialVelocity.setValue(0)
-
-        self.scope.setBackground(None)
-
-        self._plot = self.scope.getPlotItem()
-        self._plot.enableAutoRange('y', False)
-        self._plot.enableAutoRange('x', True)
-        self._plot.setDownsampling(mode='peak')
-        self._plot.setClipToView(True)
-        self._plot.setLabel('bottom', 'Time', 's')
-        self._plot.setLabel('left', 'Position', 'deg')
-        self._plot.setRange(yRange=[-(self._PRANGE + 10),
-                                    self._PRANGE + 10])
-        self._plot.showGrid(x=True, y=True)
-
-        self._curve = self.scope.getPlotItem().plot()
-        self._curve.setPen(color='y', width=2)
+        self.setState(self.stateInit)
+        self.setState(self.stateIdle)
 
         self._timerPlotUpdate = QTimer()
         self._timerPlotUpdate.timeout.connect(self.on_timerPlotUpdate_expired)
 
         self._enabled = False
 
+        # TODO: should be done asynchronously!
         self.loadAxes()
 
     def loadAxes(self):
@@ -170,6 +129,103 @@ class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
 
         self.cboxAxes.setModel(model)
 
+    def setState(self, state):
+        if state == self.stateInit:
+            uic.loadUi(join(_RESOURCES, 'ui', 'scopewindow.ui'), self)
+
+            self.splitter.setStretchFactor(0, 1)
+            self.splitter.setStretchFactor(1, 0)
+
+            self.tabsMotionControl.setCurrentIndex(self.tabHomingIndex)
+
+            palette = QApplication.instance().palette()
+            iconsColor = palette.color(QPalette.Text)
+
+            self.tabsMotionControl.setTabIcon(
+                    self.tabHomingIndex,
+                    qta.icon('fa.home', color=iconsColor))
+            self.tabsMotionControl.setTabIcon(
+                    self.tabPositionIndex,
+                    qta.icon('fa.arrows', color=iconsColor))
+            self.tabsMotionControl.setTabIcon(
+                    self.tabVelocityIndex,
+                    qta.icon('fa.tachometer', color=iconsColor))
+
+            self.dialPosition.setMinimum(-self._PRANGE)
+            self.dialPosition.setMaximum(self._PRANGE)
+            self.dialPosition.setValue(0)
+
+            self.dialVelocity.setMinimum(-self._VRANGE)
+            self.dialVelocity.setMaximum(self._VRANGE)
+            self.dialVelocity.setValue(0)
+
+            self.scope.setBackground(None)
+
+            self._plot = self.scope.getPlotItem()
+            self._plot.enableAutoRange('y', False)
+            self._plot.enableAutoRange('x', True)
+            self._plot.setDownsampling(mode='peak')
+            self._plot.setClipToView(True)
+            self._plot.setLabel('bottom', 'Time', 's')
+            self._plot.setLabel('left', 'Position', 'deg')
+            self._plot.setRange(yRange=[-(self._PRANGE + 10),
+                                        self._PRANGE + 10])
+            self._plot.showGrid(x=True, y=True)
+
+            self._curve = self.scope.getPlotItem().plot()
+            self._curve.setPen(color='y', width=2)
+
+        elif state == self.stateIdle:
+            self.cboxAxes.setEnabled(True)
+
+            self.tabHoming.setEnabled(True)
+            self.btnHoming.setEnabled(True)
+
+            self.tabPosition.setEnabled(True)
+            self.dialPosition.setEnabled(False)
+            self.btnPosition.setText('Enable')
+
+            self.tabVelocity.setEnabled(True)
+            self.dialVelocity.setEnabled(False)
+            self.btnVelocity.setText('Enable')
+
+        elif state == self.stateHoming:
+            self.cboxAxes.setEnabled(False)
+
+            self.tabPosition.setEnabled(False)
+            self.tabVelocity.setEnabled(False)
+
+            self.btnHoming.setEnabled(False)
+            self.lblHomingStatus.setText('Running...')
+
+        elif state == self.statePosition:
+            self._plot.setLabel('left', 'Position', 'deg')
+            self._plot.setRange(yRange=[-(self._PRANGE + 10),
+                                        self._PRANGE + 10])
+
+            self.cboxAxes.setEnabled(False)
+
+            self.tabHoming.setEnabled(False)
+            self.tabVelocity.setEnabled(False)
+
+            self.btnPosition.setText('Disable')
+            self.dialPosition.setEnabled(True)
+
+        elif state == self.stateVelocity:
+            self._plot.setLabel('left', 'Velocity', 'rps')
+            self._plot.setRange(yRange=[-(self._VRANGE + 10),
+                                        self._VRANGE + 10])
+
+            self.cboxAxes.setEnabled(False)
+
+            self.tabHoming.setEnabled(False)
+            self.tabPosition.setEnabled(False)
+
+            self.btnVelocity.setText('Disable')
+            self.dialVelocity.setEnabled(True)
+
+        self._state = state
+
     def currentAxis(self):
         index = self.cboxAxes.model().index(self.cboxAxes.currentIndex(), 0)
         axis = self.cboxAxes.model().data(index, Qt.UserRole)
@@ -177,7 +233,6 @@ class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
         return axis
 
     def enableScope(self, axis, reg):
-        # configure poller and plot updates
         self._poller = il.Poller(axis, reg, self._POLLER_T_S,
                                  self._POLLER_BUF_SZ)
 
@@ -196,120 +251,59 @@ class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
     @Slot(str)
     def onHomingFinished(self, result):
         self.lblHomingStatus.setText(result)
-
-        # restore GUI
-        self.cboxAxes.setEnabled(True)
-
-        self.tabPosition.setEnabled(True)
-        self.tabVelocity.setEnabled(True)
-
-        self.btnHoming.setEnabled(True)
+        self.setState(self.stateIdle)
 
     @Slot()
     def on_btnHoming_clicked(self):
-        # update GUI
-        self.cboxAxes.setEnabled(False)
+        self.setState(self.stateHoming)
 
-        self.tabPosition.setEnabled(False)
-        self.tabVelocity.setEnabled(False)
-
-        self.btnHoming.setEnabled(False)
-        self.lblHomingStatus.setText('Running...')
-
-        # run homing (asynchronously)
-        self._homingRunner = HomingRunner(self.currentAxis())
-        self._homingRunner.finished.connect(self.onHomingFinished)
-        self._homingRunner.start()
+        self._thread = QThread()
+        self._runner = HomingRunner(self.currentAxis(), self._HOMING_TIMEOUT)
+        self._runner.moveToThread(self._thread)
+        self._thread.started.connect(self._runner.run)
+        self._runner.finished.connect(self.onHomingFinished)
+        self._runner.finished.connect(self._thread.quit)
+        self._runner.finished.connect(self._runner.deleteLater)
+        self._thread.finished.connect(self._thread.deleteLater)
+        self._thread.start()
 
     @Slot()
-    def on_btnPositionPower_clicked(self):
+    def on_btnPosition_clicked(self):
         axis = self.currentAxis()
 
-        if not self._enabled:
-            # enable axis (PP mode, using degrees)
+        if self._state == self.stateIdle:
             axis.mode = il.MODE_PP
             axis.units_pos = il.UNITS_POS_DEG
             axis.enable()
 
-            # enable scope
             self.enableScope(axis, POS_ACTUAL)
-
-            # update GUI
-            self._plot.setLabel('left', 'Position', 'deg')
-            self._plot.setRange(yRange=[-(self._PRANGE + 10),
-                                        self._PRANGE + 10])
-
-            self.cboxAxes.setEnabled(False)
-
-            self.tabHoming.setEnabled(False)
-            self.tabVelocity.setEnabled(False)
-
-            self.btnPositionPower.setText('Disable')
-            self.dialPosition.setEnabled(True)
-
-            self._enabled = True
+            self.setState(self.statePosition)
         else:
-            # disable poller and plot updates
             self.disableScope()
             axis.disable()
 
-            # update GUI
-            self.cboxAxes.setEnabled(True)
-
-            self.tabHoming.setEnabled(True)
-            self.tabVelocity.setEnabled(True)
-
-            self.btnPositionPower.setText('Enable')
-            self.dialPosition.setEnabled(False)
-
-            self._enabled = False
-
-    @Slot()
-    def on_btnVelocityPower_clicked(self):
-        axis = self.currentAxis()
-
-        if not self._enabled:
-            # enable axis (PV mode, using RPS)
-            axis.mode = il.MODE_PV
-            axis.units_vel = il.UNITS_VEL_RPS
-            axis.enable()
-
-            # enable scope
-            self.enableScope(axis, VEL_ACTUAL)
-
-            # update GUI
-            self._plot.setLabel('left', 'Velocity', 'rps')
-            self._plot.setRange(yRange=[-(self._VRANGE + 10),
-                                        self._VRANGE + 10])
-
-            self.cboxAxes.setEnabled(False)
-
-            self.tabHoming.setEnabled(False)
-            self.tabPosition.setEnabled(False)
-
-            self.btnVelocityPower.setText('Disable')
-            self.dialVelocity.setEnabled(True)
-
-            self._enabled = True
-        else:
-            # disable poller and plot updates
-            self.disableScope()
-            axis.disable()
-
-            # update GUI
-            self.cboxAxes.setEnabled(True)
-
-            self.tabHoming.setEnabled(True)
-            self.tabPosition.setEnabled(True)
-
-            self.btnVelocityPower.setText('Enable')
-            self.dialVelocity.setEnabled(False)
-
-            self._enabled = False
+            self.setState(self.stateIdle)
 
     @Slot(int)
     def on_dialPosition_valueChanged(self, value):
         self.currentAxis().position = value
+
+    @Slot()
+    def on_btnVelocity_clicked(self):
+        axis = self.currentAxis()
+
+        if self._state == self.stateIdle:
+            axis.mode = il.MODE_PV
+            axis.units_vel = il.UNITS_VEL_RPS
+            axis.enable()
+
+            self.enableScope(axis, VEL_ACTUAL)
+            self.setState(self.stateVelocity)
+        else:
+            self.disableScope()
+            axis.disable()
+
+            self.setState(self.stateIdle)
 
     @Slot(int)
     def on_dialVelocity_valueChanged(self, value):
@@ -332,15 +326,10 @@ class ScopeWindow(QtBaseClass, Ui_ScopeWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    # enable High DPI support
-    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
-        app.setAttribute(Qt.AA_UseHighDpiPixmaps)
-
     # load main window (applying modern style)
     qtmodern.styles.dark(app)
 
     mw = qtmodern.windows.ModernWindow(ScopeWindow())
-    mw.setTitle('Scope')
     mw.show()
 
     sys.exit(app.exec_())
