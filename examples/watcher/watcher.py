be5c68ea74ebc9b884cfde33a463a664c1612ea5
Gerard Marull-Paretas <gmarull@ingeniamc.com>
2017-09-27 11:54:00 +0200

    examples: watcher: use a threaded updater.
    This implementation releases the event loop from the updates. Previously, the
    event loop could have suffered when having lots of registers to watch.

--- examples/watcher/watcher.py
+++ examples/watcher/watcher.py
@@ -1,6 +1,6 @@
 import sys
 
-from qtpy.QtCore import Qt, QTimer, Slot, QObject
+from qtpy.QtCore import Qt, QThread, QTimer, Signal, Slot, QObject
 from qtpy.QtGui import QStandardItemModel, QStandardItem
 from qtpy.QtWidgets import (QApplication, QDialog, QFormLayout, QLabel,
                             QDataWidgetMapper, QLineEdit)
@@ -15,41 +15,127 @@ VEL_ACTUAL = il.Register(0x606C, 0x00, il.DTYPE_S32, il.ACCESS_RW, il.PHY_VEL)
 """ Register: actual velocity. """
 
 
+class RegisterUpdater(QObject):
+    """ Register updater.
+
+        Args:
+            axis (Axis): Axis instance.
+            watched (dict): Dictionary of watched registers.
+            base_period (int, float): Updater base period.
+    """
+
+    finished = Signal()
+    """ Signal: Update finished signal. """
+
+    def __init__(self, axis, watched, base_period):
+        QObject.__init__(self)
+
+        self._axis = axis
+        self._watched = watched
+        self._base_period = base_period
+
+    @Slot()
+    def update(self):
+        """ Updates registers contents. """
+
+        for reg, cfg in self._watched.items():
+            cfg['current'] += self._base_period
+
+            if cfg['current'] >= cfg['period']:
+                cfg['curr_data'] = str(self._axis.read(reg))
+                cfg['current'] = 0
+
+        self.finished.emit()
+
+
 class RegisterWatcher(QObject):
+    """ Register watcher.
+
+        Args:
+            axis (Axis): Axis instance.
+    """
+
     def __init__(self, axis):
         QObject.__init__(self)
 
         self._axis = axis
+
         self._watched = {}
+        self._running = False
 
         self._timer = QTimer()
         self._timer.timeout.connect(self.onTimerExpired)
 
     @Slot()
-    def onTimerExpired(self):
+    def onUpdaterFinished(self):
+        """ Updates all items that changed once the updater has finished. """
+
         for reg, cfg in self._watched.items():
-            cfg['current'] += self._base_period
+                if cfg['curr_data'] != cfg['item'].data():
+                    cfg['item'].setData(cfg['curr_data'], Qt.DisplayRole)
 
-            if cfg['current'] >= cfg['period']:
-                curr_data = str(self._axis.read(reg))
-                if curr_data != cfg['item'].data():
-                    cfg['item'].setData(curr_data, Qt.DisplayRole)
+    @Slot()
+    def onTimerExpired(self):
+        """ Triggers the updater on each timer expiration. """
 
-                cfg['current'] = 0
+        self._updater.update()
 
     def start(self, base_period):
-        self._base_period = base_period
-        self._timer.start(self._base_period)
+        """ Starts the register watcher.
+
+            Args:
+                base_period (int, float): Base period.
+        """
+
+        if self._running:
+            raise RuntimeError('Watcher already started')
+
+        self._thread = QThread()
+        self._updater = RegisterUpdater(self._axis, self._watched, base_period)
+        self._updater.finished.connect(self.onUpdaterFinished)
+        self._updater.moveToThread(self._thread)
+        self._thread.start()
+
+        self._timer.start(base_period)
+
+        self._running = True
 
     def stop(self):
-        self._timer.stop()
+        """ Stops the register watcher. """
+
+        if self._running:
+            self._timer.stop()
+
+            self._thread.exit()
+            self._thread.wait()
 
     def add(self, reg, period, item):
+        """ Adds a register to the register watcher.
+
+            Args:
+                reg (Register): Register to be watched.
+                period (int, float): Update period (ms).
+                item (QStandardItem): Associated item.
+        """
+
+        if self._running:
+            raise RuntimeError('Cannot add a register while running')
+
         self._watched[reg] = {'period': period,
                               'item': item,
-                              'current': 0}
+                              'current': 0,
+                              'curr_data': ''}
 
     def remove(self, reg):
+        """ Remove a register from the register watcher.
+
+            Args:
+                reg (Register): Register to be removed.
+        """
+
+        if self._running:
+            raise RuntimeError('Cannot remove a register while running')
+
         del self._watching[reg]
 
 
