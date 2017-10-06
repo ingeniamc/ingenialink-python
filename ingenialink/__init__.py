8d07f7dcd8f975ca659c1cc4436979da483af93e
Gerard Marull-Paretas <gmarull@ingeniamc.com>
2017-10-06 15:01:55 +0200

    poller: add support for multi-channel

--- ingenialink/__init__.py
+++ ingenialink/__init__.py
@@ -1,5 +1,3 @@
-import warnings
-
 from ._ingenialink import ffi, lib
 from ._utils import _cstr, _pstr
 from . import exceptions as exc
@@ -883,26 +881,22 @@ class Poller(object):
 
         Args:
             servo (Servo): Servo.
-            reg (Register): Register to be polled.
-            period (int): Polling period (ms).
-            sz (int): Buffer size.
+            n_ch (int): Number of channels.
 
         Raises:
             IngeniaLinkCreationError: If the poller cannot be created.
     """
 
-    def __init__(self, servo, reg, period, sz):
+    def __init__(self, servo, n_ch):
         self._servo = servo
 
-        poller = lib.il_poller_create(servo._servo, reg._reg, period, sz)
+        poller = lib.il_poller_create(servo._servo, n_ch)
         _raise_null(poller)
 
         self._poller = ffi.gc(poller, lib.il_poller_destroy)
 
-        self._t = ffi.new('double **')
-        self._d = ffi.new('double **')
-        self._cnt = ffi.new('size_t *')
-        self._lost = ffi.new('int *')
+        self._n_ch = n_ch
+        self._acq = ffi.new('il_poller_acq_t **')
 
     def start(self):
         """ Start poller. """
@@ -917,20 +911,67 @@ class Poller(object):
 
     @property
     def data(self):
-        """ tuple (list, list): Current time and data vectors. """
+        """ tuple (list, list, bool): Time vector, array of data vectors and a
+            flag indicating if data was lost.
+        """
 
-        r = lib.il_poller_data_get(self._poller, self._t, self._d, self._cnt,
-                                   self._lost)
+        lib.il_poller_data_get(self._poller, self._acq)
+        acq = ffi.cast('il_poller_acq_t *', self._acq[0])
+
+        t = list(acq.t[0:acq.cnt])
+
+        d = []
+        for ch in range(self._n_ch):
+            if acq.d[ch] != ffi.NULL:
+                d.append(list(acq.d[ch][0:acq.cnt]))
+            else:
+                d.append(None)
+
+        return t, d, bool(acq.lost)
+
+    def configure(self, t_s, sz):
+        """ Configure.
+
+            Args:
+                t_s (int): Polling period (ms).
+                sz (int): Buffer size.
+        """
+
+        r = lib.il_poller_configure(self._poller, t_s, sz)
         _raise_err(r)
 
-        if self._lost[0]:
-            warnings.warn('Data was lost (increase buffer or empty faster)')
+    def ch_configure(self, ch, reg):
+        """ Configure a poller channel mapping.
 
-        t = ffi.cast('double *', self._t[0])
-        d = ffi.cast('double *', self._d[0])
-        cnt = self._cnt[0]
+            Args:
+                ch (int): Channel to be configured.
+                reg (Register): Register to associate to the given channel.
 
-        return list(t[0:cnt]), list(d[0:cnt])
+            Raises:
+                TypeError: If the register is not valid.
+        """
+
+        if not isinstance(reg, Register):
+            raise TypeError('Invalid register')
+
+        r = lib.il_poller_ch_configure(self._poller, ch, reg._reg)
+        _raise_err(r)
+
+    def ch_disable(self, ch):
+        """ Disable a channel.
+
+            Args:
+                ch (int): Channel to be disabled.
+        """
+
+        r = lib.il_poller_ch_disable(self._poller, ch)
+        _raise_err(r)
+
+    def ch_disable_all(self):
+        """ Disable all channels. """
+
+        r = lib.il_poller_ch_disable_all(self._poller)
+        _raise_err(r)
 
 
 class Monitor(object):
