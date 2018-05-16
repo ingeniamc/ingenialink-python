daa4e4f5505a583ea749652cb29e2cb27484d077
oriolbv <oriolbv@gmail.com>
2018-05-16 09:32:07 +0200

    SWLIB-68 Dictionary example added into project

--- examples/general/dictionary.py
+++ examples/general/dictionary.py
@@ -14,6 +14,16 @@ def run_example():
     for lang, label in reg.labels.items():
         print(lang, label)
 
+    net, servo = il.lucky(il.NET_PROT.MCB, dict_f='xcore.xml')
+    try:
+        servo.dict_storage_read()
+    except:
+        print("ERROR")
+
+    new_dict = servo.dict
+    new_dict.save("new_xcore_dict.xml")
+
+
 if __name__ == '__main__':
     run_example()
 
