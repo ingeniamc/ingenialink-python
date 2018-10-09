df367021745ad23459745430cb3a7b8dfb6190ca
Raul Gomez <rgomez@INT.INGENIAMC.COM>
2018-10-09 07:45:31 +0200

    SWLIB-78 fix load enums

--- examples/general/dictionary.py
+++ examples/general/dictionary.py
@@ -1,30 +1,32 @@
 import sys
+import time
+import json
 
 import ingenialink as il
 
 def run_example():
-    print("Run Example")
-
-    dct = il.Dictionary('xcore.xml')
-
-    reg = dct.regs['CONTROL_WORD']
-    print(reg)
-
-    print('Labels:')
-    for lang, label in reg.labels.items():
-        print(lang, label)
-
-    net, servo = il.lucky(il.NET_PROT.MCB, dict_f='xcore.xml')
-    try:
-        servo.dict_storage_read()
-    except:
-        print("ERROR")
-
-    new_dict = servo.dict
-    new_dict.save("new_xcore_dict.xml")
+    net, servo = il.lucky(il.NET_PROT.MCB, "summit.xml")
+    new_dict = servo.dict.regs
+    # servo.registers_enums = dict()
+    items = new_dict.items()
+    for i in range(0, 2000):
+        try:
+            for key, register in items:
+                # if register.enums_count > 0:
+                # servo.registers_enums[register.identifier] = {}
+                print(register.enums_count, register.enums)
+                # for i in range(0, register.enums_count):
+                #     enum = register.enums[i]
+                #     servo.registers_enums[register.identifier][int(enum['value'])] = str(enum['label'])
+                #     print(str(enum['label']))
+            # print(json.dumps(servo.registers_enums, sort_keys=True, indent=4))
+        except Exception as e:
+            print("error bro", e)
+            break
+    print("Bye")
 
 
 if __name__ == '__main__':
-    run_example()
+    test = run_example()
 
     sys.exit()
