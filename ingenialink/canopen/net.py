1b513d73b1fe400de086a93ab95f6853499f2363
aplans <aplans@ingeniamc.com>
2019-12-17 08:56:32 +0100

    Added heartbeat implementation to the network and a status to keep track whether or not the drive is connected

--- ingenialink/canopen/net.py
+++ ingenialink/canopen/net.py
@@ -1,9 +1,11 @@
 import time
 import canopen
 from enum import Enum
+from threading import Thread
+from time import sleep
 
 from .servo_node import Servo
-from ..net import NET_PROT
+from ..net import NET_PROT, NET_STATE
 
 class CAN_DEVICE(Enum):
     """ CAN Device. """
@@ -31,11 +33,41 @@ class CAN_BAUDRATE(Enum):
     """ 50 Kbit/s """
 
 
+class HearbeatThread(Thread):
+    def __init__(self, parent, node):
+        """ Constructor, setting initial variables """
+        super(HearbeatThread, self).__init__()
+        self.__parent = parent
+        self.__node = node
+        self.__timestamp = self.__node.nmt.timestamp
+        self.__state = NET_STATE.CONNECTED
+        self.__stop = False
+
+    def run(self):
+        while not self.__stop:
+            if self.__timestamp == self.__node.nmt.timestamp:
+                if self.__state != NET_STATE.DISCONNECTED:
+                    self.__parent.net_state = NET_STATE.DISCONNECTED
+                    self.__state = NET_STATE.DISCONNECTED
+            else:
+                if self.__state != NET_STATE.CONNECTED:
+                    self.__parent.net_state = NET_STATE.CONNECTED
+                    self.__state = NET_STATE.CONNECTED
+                self.__timestamp = self.__node.nmt.timestamp
+            sleep(1)
+
+    def activate_stop_flag(self):
+        self.__stop = True
+
+
 class Network(object):
     def __init__(self, device=None, baudrate=CAN_BAUDRATE.Baudrate_1M):
         self.__servos = []
         self.__device = device
         self.__network = canopen.Network()
+        self.__net_state = NET_STATE.DISCONNECTED
+        self.__observers = []
+        self.__heartbeat_thread = None
         if device is not None:
             try:
                 self.__network.connect(bustype=device.value[0], channel=device.value[1], bitrate=baudrate.value)
@@ -49,13 +81,40 @@ class Network(object):
             for node_id in self.__network.scanner.nodes:
                 print("Found node %d!" % node_id)
                 node = self.__network.add_node(node_id, eds)
+
+                node.nmt.start_node_guarding(1)
+
+                self.__heartbeat_thread = HearbeatThread(self, node)
+                self.__heartbeat_thread.start()
+
                 self.__servos.append(Servo(self, node, dict))
         except Exception as e:
-            print('Exceptiontrying to scan: ', e)
+            print('Exception trying to scan: ', e)
+
+    def net_state_subscribe(self, cb):
+        """ Subscribe to netowrk state changes.
+
+            Args:
+                cb: Callback
+
+            Returns:
+                int: Assigned slot.
+        """
+        r = len(self.__observers)
+        self.__observers.append(cb)
+        return r
+
+    def stop_heartbeat(self):
+        for node_id, node_obj in self.__network.nodes.items():
+            node_obj.nmt.stop_node_guarding()
+            if self.__heartbeat_thread is not None and self.__heartbeat_thread.is_alive():
+                self.__heartbeat_thread.activate_stop_flag()
+                self.__heartbeat_thread.join()
+                self.__heartbeat_thread = None
 
     def disconnect(self):
         try:
-            self.__network.bus.shutdown()
+            self.stop_heartbeat()
             self.__network.disconnect()
         except Exception as e:
             print(e)
@@ -76,3 +135,14 @@ class Network(object):
     def prot(self):
         """ NET_PROT: Obtain network protocol. """
         return NET_PROT.CAN
+
+    @property
+    def net_state(self):
+        """ Network state. """
+        return self.__net_state
+
+    @net_state.setter
+    def net_state(self, new_state):
+        self.__net_state = new_state
+        for callback in self.__observers:
+            callback(self.__net_state)
