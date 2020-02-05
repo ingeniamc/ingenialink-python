924a56a726c54dc2fc9f62ca3976be410bc5e879
Oriol Burgaya <oriol.burgaya@celeramotion.com>
2020-02-05 10:57:16 +0100

    Some improvements in motion example

--- examples/motion/basic_motion.py
+++ examples/motion/basic_motion.py
@@ -9,11 +9,12 @@ CSV_MODE = 35
 CSP_MODE = 36
 
 stop_thread = False
+sampling_time = 0.1
 
-def read_thread(servo):
+def read_thread(servo, actual_val_reg_id):
     global stop_thread
-    while(not stop_thread):
-        print("Current actual value: ", servo.read('FBK_CUR_MODULE_VALUE'))
+    while not stop_thread:
+        print("Actual value: ", servo.read(actual_val_reg_id))
 
 def slave_connection(ip):
     servo = None
@@ -34,17 +35,19 @@ def basic_motion(args):
         if args.op == 'CSV':
             op_mode = CSV_MODE
             target_reg_id = 'CL_VEL_SET_POINT_VALUE'
+            actual_val_reg_id = 'FBK_CUR_MODULE_VALUE'
             multiplier = 10
         else:
             op_mode = CSP_MODE
             target_reg_id = 'CL_POS_SET_POINT_VALUE'
+            actual_val_reg_id = 'CL_POS_FBK_VALUE'
             multiplier = 1000
 
         # Set Operation Mode
         servo.write('DRV_OP_CMD', op_mode)
 
         # Generate an slow sine wave
-        time = np.arange(0, 10, 0.1)
+        time = np.arange(0, 10, sampling_time)
         amplitude = np.sin(time)
         print("Amplitude: ", amplitude)
 
@@ -56,14 +59,14 @@ def basic_motion(args):
             return -2
 
         # Start reading thread
-        thread = threading.Thread(target=read_thread, args=(servo,))
+        thread = threading.Thread(target=read_thread, args=(servo, actual_val_reg_id))
         thread.start()
 
         # Send the generated targets
         for value in amplitude:
             print("Target demanded: ", value*multiplier)
             servo.write(target_reg_id, int(value*multiplier))
-            sleep(0.1)
+            sleep(sampling_time)
 
         # Stop the reading thread
         global stop_thread
@@ -85,6 +88,6 @@ if __name__ == '__main__':
     parser.add_argument('--ip', metavar="", type=str, help='the ip of the drive. 192.168.2.22 by default',
                         default='192.168.2.22')
     parser.add_argument('--op', metavar="", type=str, help='Operation mode to use [CSP, CSV]',
-                        default='CSV')
+                        default='CSP')
     args = parser.parse_args()
     basic_motion(args)
