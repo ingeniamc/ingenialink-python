ae2ca6cef50072bc3120bda1dc9590558748de72
Oriol Burgaya <oburgaya@ingeniamc.com>
2020-06-16 14:19:43 +0200

    Ingenialink build branch changed to testing in script

--- ingenialink/ingenialink_build.py
+++ ingenialink/ingenialink_build.py
@@ -20,7 +20,7 @@ if 'INGENIALINK_DIR' in os.environ:
     _IL_SRC = os.environ['INGENIALINK_DIR']
 else:
     _IL_URL = 'https://github.com/ingeniamc/ingenialink'
-    _IL_VER = 'next'
+    _IL_VER = 'testing'
     _IL_SRC = join(_SRC_DIR, 'ingenialink')
 
 _IL_BUILD = join(_BUILD_DIR, 'ingenialink')
