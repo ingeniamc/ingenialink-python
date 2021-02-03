41cbf3135c4c02076137d1c9cda2b850f47f450a
aplans <aplans@ingeniamc.com>
2021-02-03 15:13:19 +0100

    INGK-180 Added slave to network properties

--- ingenialink/net.py
+++ ingenialink/net.py
@@ -161,7 +161,7 @@ class Network(object):
             TypeError: If the protocol type is invalid.
             ILCreationError: If the network cannot be created.
     """
-    def __init__(self, prot, port=None, timeout_rd=0.5, timeout_wr=0.5):
+    def __init__(self, prot, port=None, slave=1, timeout_rd=0.5, timeout_wr=0.5):
         if not isinstance(prot, NET_PROT):
             raise TypeError('Invalid protocol')
 
@@ -177,6 +177,7 @@ class Network(object):
             self._net = lib.il_net_create(prot.value, opts)
             raise_null(self._net)
         else:
+            self.slave = slave
             self._net = ffi.new('il_net_t **')
 
     @classmethod
