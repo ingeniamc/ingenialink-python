226dfcd87fac52f73bbf18036a2caddd6f05ae1d
Javier Portillo <javier.portillo@novanta.com>
2021-02-26 10:47:22 +0100

    Fix setup.py install_requieres

--- setup.py
+++ setup.py
@@ -31,4 +31,8 @@ setup(name='ingenialink',
       ],
       setup_requires=['cffi>=1.0.0'],
       cffi_modules=['ingenialink/ingenialink_build.py:ffibuilder'],
-      install_requires=['cffi>=1.0.0'])
+      install_requires=[
+          'cffi>=1.0.0',
+          'numpy>=1.19.5',
+          'canopen>=1.0.0'
+      ])
