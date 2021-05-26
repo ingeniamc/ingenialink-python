1516d808357fd4d7279f982a8074d4cd674b9eab
aplans <aplans@ingeniamc.com>
2021-05-26 17:22:24 +0200

    Increase version to 5.3.2

--- ingenialink/__init__.py
+++ ingenialink/__init__.py
@@ -28,7 +28,7 @@ __all__ = ['Network', 'NetworkMonitor', 'devices', 'NET_PROT', 'NET_DEV_EVT',
            'CANOpenNetwork', 'CAN_DEVICE', 'CANOpenPoller', 'CANOpenServo',
            'err_ipb_last']
 
-__version__ = '5.3.1'
+__version__ = '5.3.2'
 
 try:
     __ingenialink_C_version__ = pstr(lib.il_version())
