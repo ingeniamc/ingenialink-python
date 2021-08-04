22dc3f55a8fc40a5aa42bee96284d3f2ead81e84
aplans <aplans@ingeniamc.com>
2021-08-04 11:48:01 +0200

    Added pytest for loading firmware

---
+++ tests/test_can_load_firmware.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_laod_firmware(connect_canopen):
+    servo, net = connect_canopen
+    assert servo is not None and net is not None
+
+    net.load_firmware(32, 'eve-net-c_1.8.1.sfu')
