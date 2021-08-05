a465f9ac241a8de4ee6960684f5e06772606454e
aplans <aplans@ingeniamc.com>
2021-08-05 11:46:01 +0200

    INGK-271 Added ethernet connection and disconnection methods

--- examples/general/eth_example.py
+++ examples/general/eth_example.py
@@ -2,15 +2,18 @@ import sys
 
 import ingenialink as il
 from ingenialink.ethernet.eth_net import EthernetNetwork
-from ingenialink.net import Network
 
 
 def eth_example():
-    network = EthernetNetwork("192.168.2.22",
-                              "eve-xcr_1.7.1.xdf",
-                              1061,
-                              il.net.NET_TRANS_PROT.UDP.value)
-    r, servo = network.connect()
+    net = EthernetNetwork()
+    servo = net.connect_to_slave("192.168.2.22",
+                                 "eve-net-c_eth_1.8.1.xdf",
+                                 1061,
+                                 il.net.NET_TRANS_PROT.UDP)
+
+    print(servo.read('DRV_ID_SOFTWARE_VERSION'))
+
+    net.disconnect_from_slave(servo)
 
 
 if __name__ == '__main__':
