20df5befc1cbd2f73e3c10459bd166fb91de6c50
Oriol Burgaya <oburgaya@ingeniamc.com>
2021-08-12 15:05:02 +0200

    INGK-283 MCB scan and connect implemented

--- ingenialink/mcb/mcb_net.py
+++ ingenialink/mcb/mcb_net.py
@@ -1,21 +1,74 @@
-from ..net import Network
+from ..net import Network, NET_PROT
+from .._ingenialink import lib, ffi
+from ingenialink.utils._utils import pstr, cstr, raise_null, raise_err
+
+from ..utils._utils import to_ms
 
 
 class MCBNetwork(Network):
-    def __init__(self, port=None, timeout_rd=0.5, timeout_wr=0.5):
-        self.__port = port
+    def __init__(self, timeout_rd=0.5, timeout_wr=0.5):
         self.__timeout_rd = timeout_rd
         self.__timeout_wr = timeout_wr
+
+        self.__net_interface = None
 
     def load_firmware(self, fw_file):
         # TODO: Implement FTP fw loader
         raise NotImplementedError
 
     def scan_nodes(self):
-        raise NotImplementedError
+        ports_found = self.devices()
+        return ports_found
 
-    def connect(self):
-        raise NotImplementedError
+    def connect(self, port=None):
+        self._on_found = ffi.NULL
+
+        callback = ffi.NULL
+        handle = ffi.NULL
+
+        opts = ffi.new('il_net_opts_t *')
+        _port = ffi.new('char []', cstr(port))
+        opts.port = _port
+        opts.timeout_rd = to_ms(self.__timeout_rd)
+        opts.timeout_wr = to_ms(self.__timeout_wr)
+
+        self.__net_interface = lib.il_net_create(NET_PROT.MCB.value, opts)
+        raise_null(self.__net_interface)
+
+        servos = lib.il_net_servos_list_get(self.__net_interface, callback, handle)
+
+        found = []
+        curr = servos
+
+        while curr:
+            found.append(int(curr.id))
+            curr = curr.next
+
+        lib.il_net_servos_list_destroy(servos)
+
+        return found
+
+    def devices(self):
+        """ Obtain a list of network devices.
+
+        Returns:
+            list: List of network devices.
+
+        Raises:
+            TypeError: If the protocol type is invalid.
+        """
+        devs = lib.il_net_dev_list_get(NET_PROT.MCB.value)
+
+        found = []
+        curr = devs
+
+        while curr:
+            found.append(pstr(curr.port))
+            curr = curr.next
+
+        lib.il_net_dev_list_destroy(devs)
+
+        return found
 
     def disconnect(self):
         raise NotImplementedError
@@ -28,14 +81,6 @@ class MCBNetwork(Network):
 
     def load_configuration(self):
         raise NotImplementedError
-
-    @property
-    def port(self):
-        return self.__port
-
-    @port.setter
-    def port(self, value):
-        self.__port = value
 
     @property
     def timeout_rd(self):
