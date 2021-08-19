8285e6719955c55d7ec0c16ed12ba6f70bbb183c
aplans <aplans@ingeniamc.com>
2021-08-19 11:14:20 +0200

    Modified ethernet load firmware example

--- examples/ethernet/eth_load_firmware.py
+++ examples/ethernet/eth_load_firmware.py
@@ -2,13 +2,13 @@ import sys
 from ingenialink.ethernet.network import EthernetNetwork
 
 
-def ecat_load_fw():
+def eth_load_fw():
     net = EthernetNetwork()
 
-    net.load_firmware(fw_file='../../firmware/eve-net-c_1.8.1.sfu',
-                      ftp_user='User', ftp_pwd='Password')
+    net.load_firmware(fw_file='../../resources/firmware/eve-net-c_1.8.1.sfu',
+                      ftp_user='Ingenia', ftp_pwd='Ingenia')
 
 
 if __name__ == '__main__':
-    ecat_load_fw()
+    eth_load_fw()
     sys.exit(0)
