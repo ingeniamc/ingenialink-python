0d8d4d23eb7da12fcf6287d650ae63a0b4b626d9
aplans <aplans@ingeniamc.com>
2021-09-07 12:45:25 +0200

    INGK-296 Refactor pytests to accept protocol flag as argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,62 +1,97 @@
+import json
 import pytest
 
-from ingenialink.network import NET_TRANS_PROT
 from ingenialink.canopen.network import CanopenNetwork, CAN_DEVICE, CAN_BAUDRATE
-from ingenialink.ethernet.network import EthernetNetwork
+from ingenialink.ethernet.network import EthernetNetwork, NET_TRANS_PROT
 from ingenialink.ethercat.network import EthercatNetwork
 from ingenialink.serial.network import SerialNetwork
 
 
+ALLOW_PROTOCOLS = ["ethernet", "ethercat", "canopen", "serial"]
+
+
+def pytest_addoption(parser):
+    parser.addoption("--protocol", action="store", default="ethernet",
+                     help=",".join(ALLOW_PROTOCOLS), choices=ALLOW_PROTOCOLS)
+
+
 @pytest.fixture
-def connect_canopen():
-    net = CanopenNetwork(device=CAN_DEVICE.IXXAT,
-                         channel=0,
-                         baudrate=CAN_BAUDRATE.Baudrate_1M)
+def read_config():
+    config = 'tests/config.json'
+    print('current config file:', config)
+    with open(config, "r") as fp:
+        contents = json.load(fp)
+    return contents
+
+
+def pytest_collection_modifyitems(config, items):
+    protocol = config.getoption("--protocol")
+    negate_protocols = [x for x in ALLOW_PROTOCOLS if x != protocol]
+    skip_by_protocol = pytest.mark.skip(reason="Protocol does not match")
+    for item in items:
+        if protocol in item.keywords:
+            continue
+        for not_protocol in negate_protocols:
+            if not_protocol in item.keywords:
+                item.add_marker(skip_by_protocol)
+
+
+def connect_canopen(protocol_contents):
+    net = CanopenNetwork(device=CAN_DEVICE(protocol_contents['device']),
+                         channel=protocol_contents['channel'],
+                         baudrate=CAN_BAUDRATE(protocol_contents['baudrate']))
 
     servo = net.connect_to_slave(
-        target=32,
-        dictionary='resources/dictionaries/eve-net-c_can_1.8.1.xdf',
-        eds='resources/dictionaries/eve-net-c_1.8.1.eds')
-    yield servo, net
-
-    net.disconnect_from_slave(servo)
+        target=protocol_contents['node_id'],
+        dictionary=protocol_contents['dictionary'],
+        eds=protocol_contents['eds'])
+    return servo, net
 
 
-@pytest.fixture
-def connect_ethernet():
+def connect_ethernet(protocol_contents):
     net = EthernetNetwork()
-    servo = net.connect_to_slave(
-        "192.168.2.22",
-        "resources/dictionaries/eve-net-c_eth_1.8.1.xdf",
-        1061,
-        NET_TRANS_PROT.UDP)
-
-    yield servo, net
-
-    net.disconnect_from_slave(servo)
-
-
-@pytest.fixture
-def connect_ethercat():
-    net = EthercatNetwork("\\Device\\NPF_{192D1D2F-C684-467D-A637-EC07BD434A63}")
 
     servo = net.connect_to_slave(
-        target=1,
-        dictionary='resources/dictionaries/eve-xcr-e_eoe_1.8.1.xdf'
+        protocol_contents['ip'],
+        protocol_contents['dictionary'],
+        protocol_contents['port'],
+        NET_TRANS_PROT[protocol_contents['protocol']])
+    return servo, net
+
+
+def connect_ethercat(protocol_contents):
+    net = EthercatNetwork(protocol_contents['ifname'])
+
+    servo = net.connect_to_slave(
+        target=protocol_contents['slave'],
+        dictionary=protocol_contents['dictionary']
     )
+    return servo, net
 
-    yield servo, net
 
-    net.disconnect_from_slave(servo)
+def connect_serial(protocol_contents):
+    net = SerialNetwork()
+
+    servo = net.connect_to_slave(
+        protocol_contents['com_port'],
+        protocol_contents['dictionary'])
+    return servo, net
 
 
 @pytest.fixture
-def connect_serial():
-    net = SerialNetwork()
-    servo = net.connect_to_slave(
-        "COM5",
-        "resources/dictionaries/eve-core_1.8.1.xdf")
+def connect_to_slave(pytestconfig, read_config):
+    servo = None
+    net = None
+    protocol = pytestconfig.getoption("--protocol")
+    protocol_contents = read_config[protocol]
+    if protocol == "ethernet":
+        servo, net = connect_ethernet(protocol_contents)
+    elif protocol == "ethercat":
+        servo, net = connect_ethercat(protocol_contents)
+    elif protocol == "canopen":
+        servo, net = connect_canopen(protocol_contents)
+    elif protocol == "serial":
+        servo, net = connect_serial(protocol_contents)
 
     yield servo, net
-
     net.disconnect_from_slave(servo)
---
+++ tests/network/test_can_network.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ingenialink.canopen.network import CanopenNetwork, CAN_DEVICE, CAN_BAUDRATE
+
+
+@pytest.mark.canopen
+def test_scan_slaves(read_config):
+    net = CanopenNetwork(device=CAN_DEVICE(read_config['canopen']['device']),
+                         channel=read_config['canopen']['channel'],
+                         baudrate=CAN_BAUDRATE(read_config['canopen']['baudrate']))
+    slaves = net.scan_slaves()
+    assert len(slaves) > 0
+
