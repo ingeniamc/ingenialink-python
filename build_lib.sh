81f48ccabde2186d65895bde7a4584ed9d785fea
aplans <aplans@ingeniamc.com>
2021-09-09 11:00:46 +0200

    INGK-316 Update to new .pyd and .c namings

--- build_lib.sh
+++ build_lib.sh
@@ -1,5 +1,5 @@
 rm -rf ./_build ./_deps ./_install ./build ./dist
 pipenv run python setup.py build sdist bdist_wheel
-rm ./ingenialink/_ingenialink.c ./ingenialink/_ingenialink.cp36-win_amd64.pyd
+rm ./ingenialink/_ingenialink.c ./ingenialink/_ingenialink.pyd
 cp ./build/temp.win-amd64-3.6/Release/ingenialink._ingenialink.c ./ingenialink/_ingenialink.c
-cp ./build/lib.win-amd64-3.6/ingenialink/_ingenialink.cp36-win_amd64.pyd ./ingenialink/_ingenialink.cp36-win_amd64.pyd
+cp ./build/lib.win-amd64-3.6/ingenialink/_ingenialink.pyd ./ingenialink/_ingenialink.pyd
