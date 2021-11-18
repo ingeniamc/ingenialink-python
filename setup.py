299838defb6f8c441ce3448f2988d9d627f46656
aplans <aplans@ingeniamc.com>
2021-11-18 16:04:02 +0100

    INGK-341 Add python 3.9 to pypi programming languages

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(name='ingenialink',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Communications',
           'Topic :: Software Development :: Libraries'
       ],
