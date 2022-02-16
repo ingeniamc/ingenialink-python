76462476ae6f009da8146f16446718ff9cdbfb2d
aplans <aplans@ingeniamc.com>
2022-02-16 12:37:22 +0100

    INGK-391 Update dependencies and setup requirements

--- setup.py
+++ setup.py
@@ -89,9 +89,9 @@ setup(name='ingenialink',
       install_requires=[
           'cffi==1.14.6',
           'numpy<=1.19.5',
-          'canopen>=1.0.0',
+          'canopen==1.2.1',
           'ingenialogger>=0.2.1'
       ],
-      include_package_data = True
+      include_package_data=True
 
       )
