e917ab0143eb6d2256ac0a92c737ac14ef5ddc14
Martin.Acosta <martin.acosta@novanta.com>
2022-06-28 15:19:46 +0200

    INGK-470 Changes to generate wheels

--- ingenialink/ingenialink_build.py
+++ ingenialink/ingenialink_build.py
@@ -20,7 +20,7 @@ if 'INGENIALINK_DIR' in os.environ:
     _IL_SRC = os.environ['INGENIALINK_DIR']
 else:
     _IL_URL = 'https://github.com/ingeniamc/ingenialink'
-    _IL_VER = '6.3.0'
+    _IL_VER = 'INGK-475-fix-windows-fatal-access-violation-error'
     _IL_SRC = join(_SRC_DIR, 'ingenialink')
 
 _IL_BUILD = join(_BUILD_DIR, 'ingenialink')
