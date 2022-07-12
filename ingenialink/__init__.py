01bf2666b6102e5fd0bb10257db33b52ed06b312
Martin.Acosta <martin.acosta@novanta.com>
2022-07-12 15:21:31 +0200

    INGK-487 Remove all references to serial communicatioon

--- ingenialink/__init__.py
+++ ingenialink/__init__.py
@@ -13,9 +13,6 @@ from .ipb.register import IPBRegister, REG_DTYPE, REG_ACCESS, REG_PHY
 from .ipb.dictionary import IPBDictionary
 from .ipb.servo import IPBServo
 
-from .serial.network import SerialNetwork
-from .serial.servo import SerialServo
-
 from .ethernet.network import EthernetNetwork
 from .ethernet.servo import EthernetServo
 
@@ -37,13 +34,12 @@ __all__ = ['EEPROM_FILE_FORMAT', 'NET_PROT', 'NET_DEV_EVT', 'NET_STATE',
            'NET_TRANS_PROT', 'SERVO_STATE', 'SERVO_FLAGS', 'SERVO_MODE',
            'SERVO_UNITS_TORQUE', 'SERVO_UNITS_POS', 'SERVO_UNITS_VEL',
            'SERVO_UNITS_ACC', 'NetworkMonitor', 'Network', 'Servo',
-           'IPBDictionary', 'IPBRegister', 'REG_DTYPE', 'REG_ACCESS', 'REG_PHY',
-           'Monitor', 'MONITOR_TRIGGER', 'IPBPoller', 'IPBServo',
-           'SerialNetwork', 'SerialServo', 'EthercatNetwork', 'EthercatServo',
-           'EthernetServo', 'EthernetNetwork',
+           'IPBDictionary', 'IPBRegister', 'REG_DTYPE', 'REG_ACCESS',
+           'REG_PHY', 'IPBPoller', 'IPBServo', 'EthercatNetwork',
+           'EthercatServo', 'EthernetServo', 'EthernetNetwork',
            'CanopenNetwork', 'CAN_DEVICE', 'CAN_BAUDRATE',
-           'CanopenServo', 'CanopenPoller', 'CanopenRegister', 'CanopenDictionary',
-           'err_ipb_last']
+           'CanopenServo', 'CanopenPoller', 'CanopenRegister',
+           'CanopenDictionary', 'err_ipb_last']
 
 __version__ = '6.3.0'
 
