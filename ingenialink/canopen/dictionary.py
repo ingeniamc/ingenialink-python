3313c2b2d54ad8efdc2315e227869cf4dc90b7a4
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2022-10-17 18:46:56 +0200

    INGK-464: redundant else is deleted, better message at exception

--- ingenialink/canopen/dictionary.py
+++ ingenialink/canopen/dictionary.py
@@ -26,31 +26,22 @@ class CanopenDictionary(Dictionary):
         self.read_dictionary()
 
     def _read_register(self, register):
-        """Reads a register from the dictionary and creates a Register instance.
-
-        Args:
-            register (Element): Register instance from the dictionary.
-
-        """
         current_read_register = super()._read_register(register)
         if current_read_register is None:
             return None
-        else:
-            try:
+        try:
 
-                aux_var = int(register.attrib['address'], 16)
-                current_read_register[self.AttrRegCanDict.IDX] = aux_var >> 8
-                current_read_register[self.AttrRegCanDict.SUBIDX] = aux_var & 0xFF
+            aux_var = int(register.attrib['address'], 16)
+            current_read_register[self.AttrRegCanDict.IDX] = aux_var >> 8
+            current_read_register[self.AttrRegCanDict.SUBIDX] = aux_var & 0xFF
 
-                return current_read_register
+            return current_read_register
 
-            except KeyError as ke:
-                logger.error(f"The register with the ID {current_read_register[self.AttrRegDict.IDENTIFIER]} had some "
-                             f"problem because of an attribute's reading. Error caught: {ke}")
-                return None
+        except KeyError as ke:
+            logger.error(f"Register with ID {current_read_register[self.AttrRegCanDict.IDENTIFIER]} has not attribute {ke}")
+            return None
 
     def _add_register_list(self, register):
-        """Adds the current read register into the _registers list"""
         identifier = register[self.AttrRegCanDict.IDENTIFIER]
         subnode = register[self.AttrRegCanDict.SUBNODE]
 
