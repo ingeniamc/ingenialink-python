5c78d28639ed7a5d3d769325e1d80bde3f3ae5a4
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2022-10-17 12:08:16 +0200

    INGK-463: constants are inside EthernetDictionary, and (**) operator is used as an input parameter

--- ingenialink/ethernet/dictionary.py
+++ ingenialink/ethernet/dictionary.py
@@ -1,13 +1,9 @@
-from ingenialink.dictionary import Dictionary, AttrRegDict
+from ingenialink.dictionary import Dictionary
 from ingenialink.ethernet.register import EthernetRegister
 
 import ingenialogger
 
 logger = ingenialogger.get_logger(__name__)
-
-
-class AttrRegEthDict(AttrRegDict):
-    ADDR = 'address'
 
 
 class EthernetDictionary(Dictionary):
@@ -17,6 +13,9 @@ class EthernetDictionary(Dictionary):
         dictionary_path (str): Path to the Ingenia dictionary.
 
     """
+
+    class AttrRegEthDict(Dictionary.AttrRegDict):
+        ADDR = 'address'
 
     def __init__(self, dictionary_path):
         super().__init__(dictionary_path)
@@ -31,7 +30,7 @@ class EthernetDictionary(Dictionary):
         try:
             current_read_register = super()._read_xdf_register(register)
 
-            current_read_register[AttrRegEthDict.ADDR] = int(register.attrib['address'], 16)
+            current_read_register[self.AttrRegEthDict.ADDR] = int(register.attrib['address'], 16)
 
             return current_read_register
 
@@ -41,23 +40,9 @@ class EthernetDictionary(Dictionary):
 
     def _add_register_list(self, register):
         """Adds the current read register into the _registers list"""
-        address = register[AttrRegEthDict.ADDR]
-        dtype = register[AttrRegEthDict.DTYPE]
-        access = register[AttrRegEthDict.ACCESS]
-        identifier = register[AttrRegEthDict.IDENTIFIER]
-        units = register[AttrRegEthDict.UNITS]
-        cyclic = register[AttrRegEthDict.CYCLIC]
-        subnode = register[AttrRegEthDict.SUBNODE]
-        storage = register[AttrRegEthDict.STORAGE]
-        reg_range = register[AttrRegEthDict.REG_RANGE]
-        labels = register[AttrRegEthDict.LABELS]
-        enums = register[AttrRegEthDict.ENUMS]
-        enums_count = len(register[AttrRegEthDict.ENUMS])
-        cat_id = register[AttrRegEthDict.CAT_ID]
-        internal_use = register[AttrRegEthDict.INT_USE]
+        identifier = register[self.AttrRegEthDict.IDENTIFIER]
+        subnode = register[self.AttrRegEthDict.SUBNODE]
 
-        reg = EthernetRegister(address, dtype, access, subnode=subnode, identifier=identifier, units=units,
-                               cyclic=cyclic, storage=storage, reg_range=reg_range, labels=labels,
-                               enums=enums, enums_count=enums_count, cat_id=cat_id, internal_use=internal_use)
+        reg = EthernetRegister(**register)
 
         self._registers[subnode][identifier] = reg
