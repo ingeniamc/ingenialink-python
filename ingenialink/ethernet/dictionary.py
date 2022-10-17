90fc9a12f540c4954cc1fc1ea669de7709219f18
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2022-10-17 18:42:27 +0200

    INGK-463: better message at exception, docstrings from protected functions are deleted

--- ingenialink/ethernet/dictionary.py
+++ ingenialink/ethernet/dictionary.py
@@ -21,30 +21,19 @@ class EthernetDictionary(Dictionary):
         super().__init__(dictionary_path)
 
     def _read_xdf_register(self, register):
-        """Reads a register from the dictionary and creates a Register instance.
-
-        Args:
-            register (Element): Register instance from the dictionary.
-
-        """
         current_read_register = super()._read_xdf_register(register)
         if current_read_register is None:
             return None
-        else:
-            try:
-                current_read_register = super()._read_xdf_register(register)
+        try:
+            current_read_register[self.AttrRegEthDict.ADDR] = int(register.attrib['address'], 16)
 
-                current_read_register[self.AttrRegEthDict.ADDR] = int(register.attrib['address'], 16)
+            return current_read_register
 
-                return current_read_register
-
-            except KeyError as ke:
-                logger.error(f"The register with the ID {current_read_register[self.AttrRegDict.IDENTIFIER]} had some "
-                             f"problem because of an attribute's reading. Error caught: {ke}")
-                return None
+        except KeyError as ke:
+            logger.error(f"Register with ID {current_read_register[self.AttrRegDict.IDENTIFIER]} has not attribute {ke}")
+            return None
 
     def _add_register_list(self, register):
-        """Adds the current read register into the _registers list"""
         identifier = register[self.AttrRegEthDict.IDENTIFIER]
         subnode = register[self.AttrRegEthDict.SUBNODE]
 
