333f3d9bf3b962866b1395c334b408b313e656f2
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2022-10-17 16:52:49 +0200

    INGK-464: new file for lack of register's attributes test, exception handling improvement

--- ingenialink/canopen/dictionary.py
+++ ingenialink/canopen/dictionary.py
@@ -32,17 +32,22 @@ class CanopenDictionary(Dictionary):
             register (Element): Register instance from the dictionary.
 
         """
-        try:
-            current_read_register = super()._read_register(register)
-            aux_var = int(register.attrib['address'], 16)
-            current_read_register[self.AttrRegCanDict.IDX] = aux_var >> 8
-            current_read_register[self.AttrRegCanDict.SUBIDX] = aux_var & 0xFF
-
-            return current_read_register
-
-        except KeyError as ke:
-            logger.error(f'Error caught: {ke}')
+        current_read_register = super()._read_register(register)
+        if current_read_register is None:
             return None
+        else:
+            try:
+
+                aux_var = int(register.attrib['address'], 16)
+                current_read_register[self.AttrRegCanDict.IDX] = aux_var >> 8
+                current_read_register[self.AttrRegCanDict.SUBIDX] = aux_var & 0xFF
+
+                return current_read_register
+
+            except KeyError as ke:
+                logger.error(f"The register with the ID {current_read_register[self.AttrRegDict.IDENTIFIER]} had some "
+                             f"problem because of an attribute's reading. Error caught: {ke}")
+                return None
 
     def _add_register_list(self, register):
         """Adds the current read register into the _registers list"""
