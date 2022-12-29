5aa4536ef5d79a9fb9950185c7c47b8ec2da3aec
Martin.Acosta <martin.acosta@novanta.com>
2022-12-29 12:29:45 +0100

    INGK-583 Add exceptions and cleanup

--- ingenialink/ethercat/network.py
+++ ingenialink/ethercat/network.py
@@ -2,7 +2,10 @@ from enum import Enum
 from ..exceptions import *
 from ..network import NET_PROT
 from .servo import EthercatServo
-from ingenialink.constants import *
+from ingenialink.constants import FILE_EXT_SFU, FILE_EXT_LFU, \
+    FORCE_BOOT_PASSWORD, FOE_WRITE_PASSWORD, FORCE_COCO_BOOT_IDX, \
+    FORCE_COCO_BOOT_SUBIDX, DEFAULT_MESSAGE_RETRIES, \
+    DEFAULT_MESSAGE_TIMEOUT
 from .._ingenialink import lib, ffi
 from ingenialink.utils._utils import cstr
 from ingenialink.ipb.network import IPBNetwork
@@ -36,7 +39,6 @@ FIRMWARE_UPDATE_ERROR = {
     lib.SOEM_EC_ERR_TYPE_EOE_INVALID_RX_DATA: 'EtherCAT Error. Invalid RX Data error'
 }
 
-
 class EEPROM_TOOL_MODE(Enum):
     """EEPROM tool mode."""
     MODE_NONE = 0
@@ -61,8 +63,9 @@ class EthercatNetwork(IPBNetwork):
         """str: Interface name used in the network settings."""
         self.servos = []
         """list: List of the connected servos in the network."""
+        self._ecat_master = None
 
-    def load_firmware(self, fw_file, slave_id=1):
+    def load_firmware(self, fw_file, slave_id=1, boot_in_app=None):
         """Loads a given firmware file to a target slave.
 
         .. warning ::
@@ -73,36 +76,50 @@ class EthercatNetwork(IPBNetwork):
         Args:
             fw_file (str): Path to the firmware file.
             slave_id (int): Slave ID to which load the firmware file.
+            boot_in_app (bool): If ``fw_file`` extension is .sfu -> True.
+                                Otherwise -> False.
 
         Raises:
             FileNotFoundError: If the firmware file cannot be found.
-            ILFirmwareLoadError: The firmware load process fails with an error message.
+            ValueError: If the firmware file has the wrong extension.
+            ILFirmwareLoadError: If no slave is detected.
+            ILFirmwareLoadError: If the FOE write operation is not
+            successful.
 
         """
         if not os.path.isfile(fw_file):
             raise FileNotFoundError(f"Could not find {fw_file}.")
-        force_boot_password = 0x424F4F54
-        firm_password = 0x70636675
-        boot_in_app = fw_file.endswith('.sfu')
-        self._soem_master = Master()
-        self._soem_master.open(self.interface_name)
-        try:
-            if self._soem_master.config_init() > 0:
-                first_slave = self._soem_master.slaves[slave_id-1]
-                if not boot_in_app:
-                    first_slave.sdo_write(0x5EDE, 0x00, force_boot_password.to_bytes(4, 'little'), False)
-                    self._set_slave_state_to_boot()
-                self._set_slave_state_to_boot()
-                with open(fw_file, 'rb') as file:
-                    file_data = file.read()
-                    r = first_slave.foe_write(os.path.basename(fw_file), firm_password, file_data)
-                    self._write_slave_state(INIT_STATE)
-            else:
-                print('no slave available')
-        except Exception as ex:
-            raise ex
-        finally:
-            self._soem_master.close()
+        if boot_in_app is None:
+            if not fw_file.endswith((FILE_EXT_SFU, FILE_EXT_LFU)):
+                raise ValueError("Firmware file should have extension "
+                                 f"{FILE_EXT_SFU} or {FILE_EXT_LFU}.")
+            boot_in_app = fw_file.endswith(FILE_EXT_SFU)
+        self._ecat_master = Master()
+        self._ecat_master.open(self.interface_name)
+        if self._ecat_master.config_init() < 0:
+            raise ILFirmwareLoadError("Firmware could not be loaded. "
+                                      "No slave detected.")
+        slave = self._ecat_master.slaves[slave_id-1]
+        if not boot_in_app:
+            slave.sdo_write(FORCE_COCO_BOOT_IDX,
+                            FORCE_COCO_BOOT_SUBIDX,
+                            FORCE_BOOT_PASSWORD.to_bytes(4, 'little'),
+                            False)
+            # COMOCO drives need to be forced two times into bootstrap state
+            self._set_slave_state_to_boot()
+        self._set_slave_state_to_boot()
+        with open(fw_file, 'rb') as file:
+            file_data = file.read()
+            file_name = os.path.basename(fw_file)
+            r = slave.foe_write(file_name,
+                                FOE_WRITE_PASSWORD,
+                                file_data)
+        self._write_slave_state(INIT_STATE)
+        self._ecat_master.close()
+        if r < 0:
+            raise ILFirmwareLoadError("The firmware file could "
+                                      "not be loaded correctly. "
+                                      f"Error code: {r}.")
 
     def _set_slave_state_to_boot(self):
         """Set all EtherCAT slaves to bootstrap state."""
@@ -117,8 +134,8 @@ class EthercatNetwork(IPBNetwork):
             state (int): State in which to set the slaves.
 
         """
-        self._soem_master.state = state
-        self._soem_master.write_state()
+        self._ecat_master.state = state
+        self._ecat_master.write_state()
 
     def _read_eeprom(self, eeprom_file, slave, file_format):
         """Reads the EEPROM.
