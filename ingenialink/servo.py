aa56b371a5407515dcd1438c04d54f7342e9d049
Javier Portillo Medina <javierportillo93@gmail.com>
2023-03-02 10:42:14 +0100

    INGK-621 Add ILTimeoutError exception in ServoStatusListener

--- ingenialink/servo.py
+++ ingenialink/servo.py
@@ -60,7 +60,7 @@ class ServoStatusListener(threading.Thread):
                     if previous_states[subnode] != current_state:
                         previous_states[subnode] = current_state
                         self.__servo._notify_state(current_state, subnode)
-                except ILIOError as e:
+                except (ILIOError, ILTimeoutError) as e:
                     logger.error("Error getting drive status. Exception : %s", e)
             time.sleep(1.5)
 
