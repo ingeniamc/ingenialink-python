4c20d011add654135ad243115fe3e844c79b64f5
Martin.Acosta <martin.acosta@novanta.com>
2023-10-17 11:21:12 +0200

    INGK-552 Split into smaller functions

--- examples/ethercat/process_data_objects.py
+++ examples/ethercat/process_data_objects.py
@@ -6,9 +6,31 @@ from ingenialink import REG_DTYPE
 from ingenialink.ethercat.network import EthercatNetwork
 from ingenialink.utils._utils import convert_bytes_to_dtype
 from ingenialink.exceptions import ILError
+from ingenialink.canopen.register import CanopenRegister
 
 
 class ProcessDataExample:
+    dtype_size = {
+        REG_DTYPE.U8: 1,
+        REG_DTYPE.S8: 1,
+        REG_DTYPE.U16: 2,
+        REG_DTYPE.S16: 2,
+        REG_DTYPE.U32: 4,
+        REG_DTYPE.S32: 4,
+        REG_DTYPE.U64: 8,
+        REG_DTYPE.S64: 8,
+        REG_DTYPE.FLOAT: 4,
+    }
+
+    rpdo_registers = [
+        "CIA402_DRV_STATE_CONTROL"
+    ]
+
+    tpdo_registers = [
+        "CIA402_CL_POS_FBK_VALUE",
+        "CIA402_CL_VEL_FBK_VALUE",
+
+    ]
 
     def __init__(self, interface_name: str, dictionary_path: str):
         self.rpdo_assign_register = 0x1C12
@@ -18,8 +40,9 @@ class ProcessDataExample:
         self.net = EthercatNetwork(interface_name)
         self.master = self.net._ecat_master
         slave = self.net.scan_slaves()[0]
-        self.net.connect_to_slave(slave, dictionary_path)
+        self.servo = self.net.connect_to_slave(slave, dictionary_path)
         self.slave = self.master.slaves[slave]
+        self.tpdo_registers_sizes = []
 
     def reset_rpdo_mapping(self) -> None:
         self.slave.sdo_write(self.rpdo_assign_register, 0x00, bytes(0))
@@ -31,16 +54,23 @@ class ProcessDataExample:
         self.slave.sdo_write(self.tpdo_map_register, 0x00, bytes(0))
 
     def map_rpdo(self) -> None:
-        rpdo_map = int.to_bytes(0x60400010, 4, "little")
-        rpdo_cnt = int.to_bytes(1, 4, "little")
+        rpdo_map = bytes()
+        for register in self.rpdo_registers:
+            rpdo_register = self.servo.dictionary.registers(1)[register]
+            rpdo_map += self.map_register(rpdo_register)
+        rpdo_cnt = int.to_bytes(len(self.rpdo_registers), 4, "little")
         self.slave.sdo_write(self.rpdo_map_register, 0x01, rpdo_map, True)
         self.slave.sdo_write(self.rpdo_map_register, 0x00, rpdo_cnt)
         self.slave.sdo_write(self.rpdo_assign_register, 0x01, self.rpdo_map_register.to_bytes(4, "little"))
         self.slave.sdo_write(self.rpdo_assign_register, 0x00, int.to_bytes(1, 4, "little"))
 
     def map_tpdo(self) -> None:
-        tpdo_map = int.to_bytes(0x60640020, 4, "little")
-        tpdo_cnt = int.to_bytes(1, 4, "little")
+        tpdo_map = bytes()
+        for register in self.tpdo_registers:
+            tpdo_register = self.servo.dictionary.registers(1)[register]
+            tpdo_map += self.map_register(tpdo_register)
+            self.tpdo_registers_sizes.append(self.dtype_size[tpdo_register.dtype])
+        tpdo_cnt = int.to_bytes(len(self.tpdo_registers), 4, "little")
         self.slave.sdo_write(self.tpdo_map_register, 0x01, tpdo_map, True)
         self.slave.sdo_write(self.tpdo_map_register, 0x00, tpdo_cnt)
         self.slave.sdo_write(self.tpdo_assign_register, 0x01, self.tpdo_map_register.to_bytes(4, "little"))
@@ -60,6 +90,29 @@ class ProcessDataExample:
                 print(f'al status code {hex(self.slave.al_status)} ({pysoem.al_status_code_to_string(self.slave.al_status)})')
                 raise ILError(f'Not all slaves reached state {state}')
 
+    def map_register(self, register: CanopenRegister):
+        index = register.idx
+        size_bytes = self.dtype_size[register.dtype]
+        mapped_register = (index << 16) | (size_bytes * 8)
+        return mapped_register.to_bytes(4, "little")
+
+    def process_inputs(self):
+        input_data = self.slave.input
+        for idx, register in enumerate(self.tpdo_registers):
+            tpdo_register = self.servo.dictionary.registers(1)[register]
+            data_size = self.tpdo_registers_sizes[idx]
+            data = input_data[:data_size]
+            input_data = input_data[data_size:]
+            value = convert_bytes_to_dtype(data, tpdo_register.dtype)
+            print(f"{register} value: {value}")
+
+    def process_data_loop(self):
+        while True:
+            self.master.send_processdata()
+            self.master.receive_processdata(2000)
+            self.process_inputs()
+            time.sleep(0.1)
+
     def run(self) -> None:
         self.slave.config_func = self.pdo_setup
         self.master.config_map()
@@ -67,17 +120,11 @@ class ProcessDataExample:
         self.master.state = pysoem.OP_STATE
         self.master.write_state()
         self.check_state(pysoem.OP_STATE)
+        print('Process data started')
         try:
-            while True:
-                # free run cycle
-                self.master.send_processdata()
-                self.master.receive_processdata(2000)
-                input_data = self.master.slaves[1].input
-                print(convert_bytes_to_dtype(input_data, REG_DTYPE.S32))
-                time.sleep(0.1)
+            self.process_data_loop()
         except KeyboardInterrupt:
-            # ctrl-C abort handling
-            print('stopped')
+            print('Process data stopped')
         self.master.state = pysoem.INIT_STATE
         self.master.write_state()
         self.master.close()
