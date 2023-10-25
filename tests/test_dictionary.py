8d40d5a19f9c42a35bb3f455588d9e273d28e973
Martin.Acosta <martin.acosta@novanta.com>
2023-10-25 11:19:31 +0200

    INGK-632 Add moco image test

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -18,8 +18,8 @@ path_resources = "./tests/resources/"
 def test_dictionary_image(dictionary_class):
     dictionary_path = join_path(path_resources, "virtual_drive.xdf")
     dictionary = dictionary_class(dictionary_path)
-    image = dictionary.image
-    assert isinstance(image, str)
+    assert isinstance(dictionary.image, str)
+    assert dictionary.moco_image is None
 
 
 @pytest.mark.parametrize("dictionary_class", [CanopenDictionary, EthernetDictionary])
@@ -39,5 +39,27 @@ def test_dictionary_image_none(dictionary_class):
     merged_file.close()
     dictionary = dictionary_class(temp_file)
     os.remove(temp_file)
-    image = dictionary.image
-    assert image is None
+    assert dictionary.image is None
+    assert dictionary.moco_image is None
+
+
+@pytest.mark.parametrize("dictionary_class", [CanopenDictionary, EthernetDictionary])
+@pytest.mark.no_connection
+def test_dictionary_moco_image(dictionary_class):
+    dictionary_path = join_path(path_resources, "virtual_drive.xdf")
+    with open(dictionary_path, "r", encoding="utf-8") as xdf_file:
+        tree = ET.parse(xdf_file)
+    root = tree.getroot()
+    image_section = root.find(dictionary_class.DICT_IMAGE)
+    image_section.set("type", "moco")
+    xml_str = minidom.parseString(ET.tostring(root)).toprettyxml(
+        indent="  ", newl="", encoding="UTF-8"
+    )
+    temp_file = join_path(path_resources, "temp.xdf")
+    merged_file = io.open(temp_file, "wb")
+    merged_file.write(xml_str)
+    merged_file.close()
+    dictionary = dictionary_class(temp_file)
+    os.remove(temp_file)
+    assert dictionary.image is None
+    assert isinstance(dictionary.moco_image, str)
