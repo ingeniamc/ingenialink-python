dab3c6baf51d4ab14ce7d97262f12016f15418e4
Pablo Zinemanas <pablo.zinemanas@novanta.com>
2024-04-04 14:22:27 +0200

    INGK-791 Add dictionary path as an argument

--- examples/canopen/can_connection_linux.py
+++ examples/canopen/can_connection_linux.py
@@ -3,17 +3,19 @@ import sys
 from ingenialink.canopen.network import CAN_BAUDRATE, CAN_DEVICE, CanopenNetwork
 
 
-def connection_example():
+def connection_example(dict_path: str) -> None:
     """Scans for nodes in a network, connects to the first found node, reads
-    a register and disconnects the found servo from the network."""
+    a register and disconnects the found servo from the network.
+
+    Args:
+        dict_path: Path to the dictionary
+    """
     net = CanopenNetwork(device=CAN_DEVICE.SOCKETCAN, channel=0, baudrate=CAN_BAUDRATE.Baudrate_1M)
     nodes = net.scan_slaves()
     print(nodes)
 
     if len(nodes) > 0:
-        servo = net.connect_to_slave(
-            target=nodes[0], dictionary="../../resources/dictionaries/eve-net-c_can_1.8.1.xdf"
-        )
+        servo = net.connect_to_slave(target=nodes[0], dictionary=dict_path)
 
         fw_version = servo.read("DRV_ID_SOFTWARE_VERSION")
         print(fw_version)
@@ -24,5 +26,6 @@ def connection_example():
 
 
 if __name__ == "__main__":
-    connection_example()
+    dict_path = "../../resources/dictionaries/eve-net-c_can_1.8.1.xdf"
+    connection_example(dict_path)
     sys.exit()
