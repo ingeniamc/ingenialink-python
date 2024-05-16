ecd4c8ec9f39110df0d83017e3885ddc5c5ddb8c
Pablo Zinemanas <pablo.zinemanas@novanta.com>
2024-05-16 11:16:48 +0200

    INGK-892 Add boot_in_app flag in the load firmware of Ethercat

--- ingenialink/ethercat/network.py
+++ ingenialink/ethercat/network.py
@@ -409,12 +409,13 @@ class EthercatNetwork(Network):
             self.__listener_net_status.join()
         self.__listener_net_status = None
 
-    def load_firmware(self, fw_file: str, slave_id: int = 1) -> None:  # type: ignore [override]
+    def load_firmware(self, fw_file: str, slave_id: int = 1, boot_in_app=False) -> None:  # type: ignore [override]
         """Loads a given firmware file to a target slave.
 
         Args:
             fw_file: Path to the firmware file.
             slave_id: Slave ID to which load the firmware file.
+            boot_in_app: True if the application includes FoE (i.e, ``fw_file`` extension is .sfu), False otherwise.
 
         Raises:
             FileNotFoundError: If the firmware file cannot be found.
@@ -449,14 +450,14 @@ class EthercatNetwork(Network):
         try:
             if sys_name == "linux":
                 subprocess.run(
-                    f"{exec_path} {self.interface_name} {slave_id} {fw_file}",
+                    f"{exec_path} {self.interface_name} {slave_id} {fw_file} {int(boot_in_app)}",
                     check=True,
                     shell=True,
                     encoding="utf-8",
                 )
             else:
                 subprocess.run(
-                    [exec_path, self.interface_name, f"{slave_id}", fw_file],
+                    [exec_path, self.interface_name, f"{slave_id}", fw_file, int(boot_in_app)],
                     check=True,
                     shell=True,
                     encoding="utf-8",
