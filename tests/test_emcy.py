0aee1b6083c950f0ec6429c93f4da8ed2beb39cc
Martin Acosta <martinacostallano@gmail.com>
2024-10-30 11:17:40 +0100

    INGK-993 Create a separate EMCY test for COCO MOCO drives

--- tests/test_emcy.py
+++ tests/test_emcy.py
@@ -2,6 +2,8 @@ import pytest
 
 from ingenialink.exceptions import ILError
 
+EVEREST_PROJECT_NUMBER = 44
+
 
 class EmcyTest:
     def __init__(self):
@@ -11,10 +13,17 @@ class EmcyTest:
         self.messages.append(emcy_msg)
 
 
+def is_coco_moco(servo):
+    product_code = servo.read("DRV_ID_PRODUCT_CODE")
+    return product_code >> 20 == EVEREST_PROJECT_NUMBER
+
+
 @pytest.mark.canopen
 @pytest.mark.ethercat
 def test_emcy_callback(connect_to_slave):
     servo, _ = connect_to_slave
+    if is_coco_moco(servo):
+        pytest.skip("The test is not supported for COCO MOCO drives")
     emcy_test = EmcyTest()
     servo.emcy_subscribe(emcy_test.emcy_callback)
     prev_val = servo.read("DRV_PROT_USER_OVER_VOLT", subnode=1)
@@ -29,5 +38,28 @@ def test_emcy_callback(connect_to_slave):
     second_emcy = emcy_test.messages[1]
     assert second_emcy.error_code == 0x0000
     assert second_emcy.get_desc() == "No error"
+    servo.write("DRV_PROT_USER_OVER_VOLT", data=prev_val, subnode=1)
+    servo.emcy_unsubscribe(emcy_test.emcy_callback)
+
+
+@pytest.mark.canopen
+@pytest.mark.ethercat
+def test_emcy_callback_coco_moco(connect_to_slave):
+    # EMCY test for COCO MOCO drives
+    # Check INGK-993
+    servo, _ = connect_to_slave
+    if not is_coco_moco(servo):
+        pytest.skip("The test is only for COCO MOCO drives")
+    emcy_test = EmcyTest()
+    servo.emcy_subscribe(emcy_test.emcy_callback)
+    prev_val = servo.read("DRV_PROT_USER_OVER_VOLT", subnode=1)
+    servo.write("DRV_PROT_USER_OVER_VOLT", data=10.0, subnode=1)
+    with pytest.raises(ILError):
+        servo.enable()
+    try:
+        servo.fault_reset()
+    except ILError:
+        servo.fault_reset()
+    assert len(emcy_test.messages) > 0
     servo.write("DRV_PROT_USER_OVER_VOLT", data=prev_val, subnode=1)
     servo.emcy_unsubscribe(emcy_test.emcy_callback)
