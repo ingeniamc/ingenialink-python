69305855cadacdb77bc8551faad5f034db082c23
Martin Acosta <martinacostallano@gmail.com>
2024-11-06 18:34:08 +0100

    INGK-1000 Ignore ruff errors

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -21,6 +21,8 @@ from ingenialink.register import REG_ADDRESS_TYPE
 from ingenialink.servo import SERVO_STATE
 from tests.virtual.test_virtual_network import (
     RESOURCES_FOLDER,
+    connect_virtual_drive,  # noqa: F401
+    stop_virtual_drive,  # noqa: F401
 )
 
 MONITORING_CH_DATA_SIZE = 4
@@ -250,7 +252,7 @@ def test_load_configuration(connect_to_slave, read_config, pytestconfig):
 
 @pytest.mark.no_connection
 @pytest.mark.usefixtures("stop_virtual_drive")
-def test_load_configuration_strict(mocker, connect_virtual_drive):
+def test_load_configuration_strict(mocker, connect_virtual_drive):  # noqa: F811
     dictionary = os.path.join(RESOURCES_FOLDER, "virtual_drive.xdf")
     servo, net = connect_virtual_drive(dictionary)
     test_file = "./tests/resources/test_config_file.xcf"
@@ -618,7 +620,7 @@ def test_disturbance_overflow(connect_to_slave, pytestconfig):
 
 
 @pytest.mark.no_connection
-def test_subscribe_register_updates(connect_virtual_drive):
+def test_subscribe_register_updates(connect_virtual_drive):  # noqa: F811
     user_over_voltage_uid = "DRV_PROT_USER_OVER_VOLT"
     register_update_callback = RegisterUpdateTest()
 
