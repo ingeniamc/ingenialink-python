49849a05a22ca38f4fb7a056c5ebfd5d7496b24e
Martin Acosta <martinacostallano@gmail.com>
2025-01-09 09:48:13 +0100

    INGK-1014 Add new test

--- tests/ethernet/test_ethernet_servo.py
+++ tests/ethernet/test_ethernet_servo.py
@@ -7,7 +7,7 @@ import pytest
 @pytest.mark.ethernet()
 @pytest.mark.parametrize(
     ("ip_address", "gateway"),
-    [("192.168.2.xx", "192.168.2.1"), ("192.168.2.22", "192.168.3.1")],
+    [("192.168.2.22", "192.168.3.1")],
 )
 def test_change_tcp_ip_parameters_value_error(connect_to_slave, ip_address, gateway):
     servo, net = connect_to_slave
@@ -16,6 +16,20 @@ def test_change_tcp_ip_parameters_value_error(connect_to_slave, ip_address, gate
         match=re.escape(
             f"Drive IP {ip_address} and Gateway IP {gateway} are not on the same network."
         ),
+    ):
+        servo.change_tcp_ip_parameters(ip_address, "255.255.255.0", gateway)
+
+
+@pytest.mark.ethernet()
+@pytest.mark.parametrize(
+    ("ip_address", "gateway"),
+    [("192.168.2.xx", "192.168.2.1")],
+)
+def test_change_tcp_ip_parameters_invalid_ip(connect_to_slave, ip_address, gateway):
+    servo, net = connect_to_slave
+    with pytest.raises(
+        ValueError,
+        match=re.escape(f"{ip_address} does not appear to be an IPv4 or IPv6 address"),
     ):
         servo.change_tcp_ip_parameters(ip_address, "255.255.255.0", gateway)
 
