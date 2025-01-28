3a2b90d106c39b80f42b8f289095196bdd055569
julieta.prieto <julieta.prieto@novanta.com>
2025-01-28 08:29:36 +0100

    INGK-1022: deprecation warning when importing deprecated aliases

--- ingenialink/__init__.py
+++ ingenialink/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from ingenialink.enums.register import RegAccess, RegDtype, RegPhy
 from ingenialink.enums.servo import (
     ServoFlags,
@@ -20,70 +22,68 @@ from .ethernet.network import EthernetNetwork
 from .ethernet.servo import EthernetServo
 from .network import EepromFileFormat, NetDevEvt, NetProt, NetState, NetTransProt, Network
 
-# WARNING: Deprecated aliases
-NET_PROT = NetProt
-NET_STATE = NetState
-NET_DEV_EVT = NetDevEvt
-EEPROM_FILE_FORMAT = EepromFileFormat
-NET_TRANS_PROT = NetTransProt
-REG_DTYPE = RegDtype
-REG_ACCESS = RegAccess
-REG_PHY = RegPhy
-SERVO_STATE = ServoState
-SERVO_FLAGS = ServoFlags
-SERVO_MODE = ServoMode
-SERVO_UNITS_TORQUE = ServoUnitsTorque
-SERVO_UNITS_POS = ServoUnitsPos
-SERVO_UNITS_VEL = ServoUnitsVel
-SERVO_UNITS_ACC = ServoUnitsAcc
-CAN_DEVICE = CanDevice
-CAN_BAUDRATE = CanBaudrate
-
 __all__ = [
-    "EEPROM_FILE_FORMAT",  # WARNING: deprecated
     "EepromFileFormat",
-    "NET_PROT",  # WARNING: deprecated
     "NetProt",
-    "NET_DEV_EVT",  # WARNING: deprecated
     "NetDevEvt",
-    "NET_STATE",  # WARNING: deprecated
     "NetState",
-    "NET_TRANS_PROT",  # WARNING: deprecated
     "NetTransProt",
-    "SERVO_STATE",  # WARNING: deprecated
     "ServoState",
-    "SERVO_FLAGS",  # WARNING: deprecated
     "ServoFlags",
-    "SERVO_MODE",  # WARNING: deprecated
     "ServoMode",
-    "SERVO_UNITS_TORQUE",  # WARNING: deprecated
     "ServoUnitsTorque",
-    "SERVO_UNITS_POS",  # WARNING: deprecated
     "ServoUnitsPos",
-    "SERVO_UNITS_VEL",  # WARNING: deprecated
     "ServoUnitsVel",
-    "SERVO_UNITS_ACC",  # WARNING: deprecated
     "ServoUnitsAcc",
     "Network",
     "Servo",
-    "REG_DTYPE",  # WARNING: deprecated
     "RegDtype",
-    "REG_ACCESS",  # WARNING: deprecated
     "RegAccess",
-    "REG_PHY",  # WARNING: deprecated
     "RegPhy",
     "EthercatNetwork",
     "EthernetServo",
     "EthernetNetwork",
     "CanopenNetwork",
-    "CAN_DEVICE",  # WARNING: deprecated
     "CanDevice",
-    "CAN_BAUDRATE",  # WARNING: deprecated
     "CanBaudrate",
     "CanopenServo",
     "CanopenRegister",
     "Poller",
     "CanopenDictionaryV2",
 ]
+
+
+# WARNING: Deprecated aliases
+_DEPRECATED = {
+    "NET_PROT": "NetProt",
+    "NET_STATE": "NetState",
+    "NET_DEV_EVT": "NetDevEvt",
+    "EEPROM_FILE_FORMAT": "EepromFileFormat",
+    "NET_TRANS_PROT": "NetTransProt",
+    "REG_DTYPE": "RegDtype",
+    "REG_ACCESS": "RegAccess",
+    "REG_PHY": "RegPhy",
+    "SERVO_STATE": "ServoState",
+    "SERVO_FLAGS": "ServoFlags",
+    "SERVO_MODE": "ServoMode",
+    "SERVO_UNITS_TORQUE": "ServoUnitsTorque",
+    "SERVO_UNITS_POS": "ServoUnitsPos",
+    "SERVO_UNITS_VEL": "ServoUnitsVel",
+    "SERVO_UNITS_ACC": "ServoUnitsAcc",
+    "CAN_DEVICE": "CanDevice",
+    "CAN_BAUDRATE": "CanBaudrate",
+}
+
+
+def __getattr__(name):
+    if name in _DEPRECATED:
+        warnings.warn(
+            f"{name} is deprecated, use {_DEPRECATED[name]} instead",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return globals()[_DEPRECATED[name]]
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
 
 __version__ = "7.4.0"
