77520420afcebaba68400467ac50e247a167ba09
julieta.prieto <julieta.prieto@novanta.com>
2025-02-17 15:43:11 +0100

    pysoem forked version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
         "python-can==4.4.2",
         "ingenialogger>=0.2.1",
         "ping3==4.0.3",
-        "pysoem>=1.1.10, <1.2.0",
+        "pysoem @ git+https://github.com/julieta-prieto/pysoem@97d8545#egg=pysoem",
         "numpy>=1.26.0",
         "scipy==1.12.0",
         "bitarray==2.9.2",
