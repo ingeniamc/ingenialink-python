44c063336c36b9e9bc8a0c838a11af21685500de
julieta.prieto <julieta.prieto@novanta.com>
2025-02-17 12:53:56 +0100

    fix test

--- tests/test_get_adapters_addresses.py
+++ tests/test_get_adapters_addresses.py
@@ -5,12 +5,13 @@ import pytest
 
 # Module will only be available for Windows
 with contextlib.suppress(ImportError):
-    pass
+    from ingenialink.get_adapters_addresses import get_adapters_addresses
 
 
 @pytest.mark.skipif(
-    platform.system != "Windows", reason="Skipping GetAdaptersAddresses, only available on Windows"
+    platform.system() != "Windows",
+    reason=f"Skipping test, only available on Windows, platform={platform.system()}",
 )
 def test_get_adapters_addresses():
-    addresses = []
+    addresses = get_adapters_addresses()
     assert not len(addresses)
