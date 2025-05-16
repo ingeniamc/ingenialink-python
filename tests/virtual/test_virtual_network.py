9e3d070f5cee208239a0656814242f3f7771584e
julieta.prieto <julieta.prieto@novanta.com>
2025-05-16 13:46:46 +0200

    fix test_connect_virtual_custom_dictionaries

--- tests/virtual/test_virtual_network.py
+++ tests/virtual/test_virtual_network.py
@@ -1,7 +1,9 @@
 import os
+import random
 
 import pytest
 
+from ingenialink.enums.register import RegAccess, RegDtype
 from ingenialink.exceptions import ILNACKError
 from ingenialink.network import NetState
 from virtual_drive.core import VirtualDrive
@@ -29,33 +31,36 @@ def test_virtual_drive_disconnection(virtual_drive_custom_dict, virtual_drive_re
     assert servo.socket._closed
 
 
-# TODO: testing with hard coded dictionary paths, do we need this?
-# @pytest.mark.no_connection
-# def test_connect_virtual_custom_dictionaries(virtual_drive_custom_dict, read_config):
-#     config = read_config
-#     for protocol in ["ethernet", "canopen"]:
-#         dictionary = config[protocol]["dictionary"]
-#         if not os.path.exists(dictionary):
-#             continue
-#         server, net, servo = virtual_drive_custom_dict(dictionary)
-#         assert servo is not None and net is not None
-#         assert len(net.servos) == 1
-#         fw_version = servo.read("DRV_ID_SOFTWARE_VERSION")
-#         assert fw_version is not None and fw_version != ""
+@pytest.mark.no_connection
+def test_connect_virtual_custom_dictionaries(virtual_drive_custom_dict):
+    dictionaries = [
+        "//azr-srv-ingfs1/pool/distext/products/EVE-XCR/firmware/2.4.0/eve-xcr-c_eth_2.4.0.xdf",
+        "//azr-srv-ingfs1/pool/distext/products/CAP-XCR/firmware/2.4.0/cap-xcr-c_eth_2.4.0.xdf",
+        "//azr-srv-ingfs1/pool/distext/products/EVE-XCR/firmware/2.4.0/eve-xcr-c_can_2.4.0.xdf",
+        "//azr-srv-ingfs1/pool/distext/products/CAP-XCR/firmware/2.4.0/cap-xcr-c_can_2.4.0.xdf",
+    ]
+    for dictionary in dictionaries:
+        if not os.path.exists(dictionary):
+            continue
+        server, net, servo = virtual_drive_custom_dict(dictionary)
+        assert servo is not None and net is not None
+        assert len(net.servos) == 1
+        fw_version = servo.read("DRV_ID_SOFTWARE_VERSION")
+        assert fw_version is not None and fw_version != ""
 
-#         for reg_key, register in servo.dictionary.registers(1).items():
-#             if register.access in [RegAccess.RO, RegAccess.RW]:
-#                 value = servo.read(reg_key)
-#                 assert pytest.approx(server.get_value_by_id(1, reg_key), abs=0.02) == value
+        for reg_key, register in servo.dictionary.registers(1).items():
+            if register.access in [RegAccess.RO, RegAccess.RW]:
+                value = servo.read(reg_key)
+                assert pytest.approx(server.get_value_by_id(1, reg_key), abs=0.02) == value
 
-#             if register.access in [RegAccess.WO, RegAccess.RW]:
-#                 if register.enums_count > 0:
-#                     continue
-#                 value = random.uniform(0, 100)
-#                 if register.dtype != RegDtype.FLOAT:
-#                     value = int(value)
-#                 servo.write(reg_key, value)
-#                 assert pytest.approx(value) == server.get_value_by_id(1, reg_key)
+            if register.access in [RegAccess.WO, RegAccess.RW]:
+                if register.enums_count > 0:
+                    continue
+                value = random.uniform(0, 100)
+                if register.dtype != RegDtype.FLOAT:
+                    value = int(value)
+                servo.write(reg_key, value)
+                assert pytest.approx(value) == server.get_value_by_id(1, reg_key)
 
 
 @pytest.mark.no_connection
