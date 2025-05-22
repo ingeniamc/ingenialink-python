08fed88e45cfe508e03792a0e789fe9f2137cf2a
julieta.prieto <julieta.prieto@novanta.com>
2025-05-22 10:39:51 +0200

    pytest fix

--- tests/test_drive_context_manager.py
+++ tests/test_drive_context_manager.py
@@ -93,14 +93,14 @@ def test_drive_context_manager_skips_default_do_not_restore_registers(setup_mana
             assert uid in context._do_not_restore_registers
 
             previous_reg_value = servo.read(uid, subnode=axis)
-            assert previous_reg_value != pwd
 
             with context:
                 servo.write(reg=uid, data=pwd, subnode=axis)
-                assert servo.read(uid, subnode=axis) == pwd
+                new_reg_value = servo.read(uid, subnode=axis)
+                assert new_reg_value != previous_reg_value
 
             # Context do not restore the register
-            assert servo.read(uid, subnode=axis) == pwd
+            assert servo.read(uid, subnode=axis) == new_reg_value
 
 
 @pytest.mark.ethernet
