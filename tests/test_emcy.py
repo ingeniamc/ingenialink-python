051e420b47f310725c4309b28a8ed4b00e791fab
julieta.prieto <julieta.prieto@novanta.com>
2025-05-28 14:12:41 +0200

    use context instead of manual value restore

--- tests/test_emcy.py
+++ tests/test_emcy.py
@@ -26,7 +26,6 @@ def test_emcy_callback(servo):
         pytest.skip("The test is not supported for COCO MOCO EtherCAT drives")
     emcy_test = EmcyTest()
     servo.emcy_subscribe(emcy_test.emcy_callback)
-    prev_val = servo.read("DRV_PROT_USER_OVER_VOLT", subnode=1)
     servo.write("DRV_PROT_USER_OVER_VOLT", data=10.0, subnode=1)
     with pytest.raises(ILError):
         servo.enable()
@@ -38,7 +37,6 @@ def test_emcy_callback(servo):
     second_emcy = emcy_test.messages[1]
     assert second_emcy.error_code == 0x0000
     assert second_emcy.get_desc() == "No error"
-    servo.write("DRV_PROT_USER_OVER_VOLT", data=prev_val, subnode=1)
     servo.emcy_unsubscribe(emcy_test.emcy_callback)
 
 
