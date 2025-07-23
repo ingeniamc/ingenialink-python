1a78b14dee072eeb010590c39758372c881a3ad0
Pol Feliu <pol.feliu@novanta.com>
2025-07-23 18:31:11 +0200

    INGK-1110 fix rel import error

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 
-from . import canopen as canopen
-from . import comkit as comkit
-from . import ethercat as ethercat
-from . import ethernet as ethernet
+from tests.resources import canopen, comkit, ethercat, ethernet
 
 resources_path = absolute_module_path = Path(__file__).parent
 
@@ -11,3 +8,15 @@ DEN_NET_E_2_8_0_xdf_v3 = (resources_path / "den-net-e_2.8.0_v3.xdf").as_posix()
 TEST_DICT_ECAT_EOE_SAFE_v3 = (resources_path / "test_dict_ecat_eoe_safe_v3.0.xdf").as_posix()
 TEST_DICT_ECAT_EOE_v3 = (resources_path / "test_dict_ecat_eoe_v3.0.xdf").as_posix()
 TEST_CONFIG_FILE = (resources_path / "test_config_file.xcf").as_posix()
+
+
+__all__ = [
+    "DEN_NET_E_2_8_0_xdf_v3",
+    "TEST_DICT_ECAT_EOE_SAFE_v3",
+    "TEST_DICT_ECAT_EOE_v3",
+    "TEST_CONFIG_FILE",
+    "canopen",
+    "comkit",
+    "ethercat",
+    "ethernet",
+]
