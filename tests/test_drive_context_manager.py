759269258c047469091452d1e6b48903eb323bec
MartinALL91 <martinacostallano@gmail.com>
2025-12-03 17:21:24 +0100

    INGK-1223 Remove virtual drive tests

--- tests/test_drive_context_manager.py
+++ tests/test_drive_context_manager.py
@@ -27,7 +27,6 @@ def _read_user_under_voltage_uid(servo):
 @pytest.mark.ethernet
 @pytest.mark.ethercat
 @pytest.mark.canopen
-@pytest.mark.virtual
 def test_drive_context_manager(
     setup_manager: tuple["Network", Union[str, list[str]], "DriveEnvironmentController"],
 ):
@@ -59,7 +58,6 @@ def test_drive_context_manager(
 @pytest.mark.ethernet
 @pytest.mark.ethercat
 @pytest.mark.canopen
-@pytest.mark.virtual
 def test_drive_context_manager_nested_contexts(
     setup_manager: tuple["Network", Union[str, list[str]], "DriveEnvironmentController"],
 ):
@@ -95,7 +93,6 @@ def test_drive_context_manager_nested_contexts(
 @pytest.mark.ethernet
 @pytest.mark.ethercat
 @pytest.mark.canopen
-@pytest.mark.virtual
 def test_drive_context_manager_skips_default_do_not_restore_registers(
     setup_manager: tuple["Network", Union[str, list[str]], "DriveEnvironmentController"],
 ):
@@ -119,7 +116,6 @@ def test_drive_context_manager_skips_default_do_not_restore_registers(
 @pytest.mark.ethernet
 @pytest.mark.ethercat
 @pytest.mark.canopen
-@pytest.mark.virtual
 def test_drive_context_manager_with_do_not_restore_registers(
     setup_manager: tuple["Network", Union[str, list[str]], "DriveEnvironmentController"],
 ):
