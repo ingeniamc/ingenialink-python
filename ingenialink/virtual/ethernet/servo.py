c58f58ae215616f29030ab8edf90149cc6562144
MartinALL91 <martinacostallano@gmail.com>
2026-02-18 15:34:26 +0100

    INGK-1245 Remove type ignore

--- ingenialink/virtual/ethernet/servo.py
+++ ingenialink/virtual/ethernet/servo.py
@@ -1,3 +1,7 @@
+import socket
+from typing import Callable, Optional
+
+from ingenialink import Servo
 from ingenialink.dictionary import Interface
 from ingenialink.ethernet.servo import EthernetServoBase
 from ingenialink.virtual.servo import VirtualServoBase
@@ -6,8 +10,21 @@ from ingenialink.virtual.servo import VirtualServoBase
 class VirtualEthernetServo(EthernetServoBase):
     """Virtual Ethernet servo implementation."""
 
-    interface = Interface.VIRTUAL
+    interface = Interface.ETH
 
-    def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        socket: socket.socket,
+        dictionary_path: str,
+        servo_status_listener: bool = False,
+        is_eoe: bool = False,
+        disconnect_callback: Optional[Callable[[Servo], None]] = None,
+    ) -> None:
+        super().__init__(
+            socket,
+            dictionary_path,
+            servo_status_listener,
+            is_eoe,
+            disconnect_callback,
+        )
         self._virtual_base = VirtualServoBase()
