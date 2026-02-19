aa93149b1eb412c3a33e81d22a7d5d407f9854d4
MartinALL91 <martinacostallano@gmail.com>
2026-02-19 12:40:02 +0100

    INGK-1245 Use absolute imports

--- ingenialink/virtual/ethercat/__init__.py
+++ ingenialink/virtual/ethercat/__init__.py
@@ -1,4 +1,4 @@
-from .network import VirtualEthercatNetwork
-from .servo import VirtualEthercatServo
+from ingenialink.virtual.ethercat.network import VirtualEthercatNetwork
+from ingenialink.virtual.ethercat.servo import VirtualEthercatServo
 
 __all__ = ["VirtualEthercatNetwork", "VirtualEthercatServo"]
--- ingenialink/virtual/ethernet/__init__.py
+++ ingenialink/virtual/ethernet/__init__.py
@@ -1,4 +1,4 @@
-from .network import VirtualEthernetNetwork
-from .servo import VirtualEthernetServo
+from ingenialink.virtual.ethernet.network import VirtualEthernetNetwork
+from ingenialink.virtual.ethernet.servo import VirtualEthernetServo
 
 __all__ = ["VirtualEthernetNetwork", "VirtualEthernetServo"]
