f6352f3488f5b5b9e11f465c8ddc16e3fc08c524
julieta.prieto <julieta.prieto@novanta.com>
2026-02-24 11:49:26 +0100

    INGK-1248 update latest version of all products

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -17,21 +17,40 @@ __TEST_CONFIGS_KEY: str = "test_configs"
 __CONFIG_FILES_PATH: Path = Path(__file__).parent / "config_files"
 
 ECAT_SETUP = SpecifierContainer({
-    PartNumber.EVE_XCR_E: RackServiceConfigSpecifier.from_firmware(
+    PartNumber.EVE_XCR_E: RackServiceConfigSpecifier.from_version_configs(
         part_number=PartNumber.EVE_XCR_E,
         interface=Interface.ECAT,
-        config_file=__CONFIG_FILES_PATH / "ethercat/eve_xcr_e.xcf",
-        version="2.6.0",
-        dictionary_type=DictionaryType.XDF_V2,
-        extra_data={
-            __EXECUTION_POLICY_KEY: "always",
-            __TEST_CONFIGS_KEY: {
-                "ECAT_TEST_SESSIONS": PyTestConfig(
-                    markers="ethercat",
-                    run_test_stage_uid="ethercat_everest",
-                    stage_name="EtherCAT Everest",
-                )
-            },
+        version_configs={
+            "2.6.0": VersionConfig.from_version(
+                version="2.6.0",
+                config_file=__CONFIG_FILES_PATH / "ethercat/eve_xcr_e.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ECAT_TEST_SESSIONS": PyTestConfig(
+                            markers="ethercat",
+                            run_test_stage_uid="ethercat_everest",
+                            stage_name="EtherCAT Everest - FW. 2.6.0",
+                        )
+                    },
+                },
+            ),
+            "2.8.0": VersionConfig.from_version(
+                version="2.8.0",
+                config_file=__CONFIG_FILES_PATH / "ethercat/eve_xcr_e.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ECAT_TEST_SESSIONS": PyTestConfig(
+                            markers="ethercat",
+                            run_test_stage_uid="ethercat_everest",
+                            stage_name="EtherCAT Everest - FW. 2.8.0",
+                        )
+                    },
+                },
+            ),
         },
     ),
     PartNumber.CAP_XCR_E: RackServiceConfigSpecifier.from_version_configs(
@@ -73,75 +92,151 @@ ECAT_SETUP = SpecifierContainer({
 })
 
 ETH_SETUP = SpecifierContainer({
-    PartNumber.EVE_XCR_C: RackServiceConfigSpecifier.from_firmware(
+    PartNumber.EVE_XCR_C: RackServiceConfigSpecifier.from_version_configs(
         part_number=PartNumber.EVE_XCR_C,
         interface=Interface.ETH,
-        config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
-        version="2.4.0",
-        dictionary_type=DictionaryType.XDF_V2,
-        extra_data={
-            __EXECUTION_POLICY_KEY: "always",
-            __TEST_CONFIGS_KEY: {
-                "ETH_TEST_SESSIONS": PyTestConfig(
-                    markers="ethernet",
-                    run_test_stage_uid="ethernet_everest",
-                    stage_name="Ethernet Everest",
-                )
-            },
+        version_configs={
+            "2.4.0": VersionConfig.from_version(
+                version="2.4.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ETH_TEST_SESSIONS": PyTestConfig(
+                            markers="ethernet",
+                            run_test_stage_uid="ethernet_everest",
+                            stage_name="Ethernet Everest - FW. 2.4.0",
+                        )
+                    },
+                },
+            ),
+            "2.8.0": VersionConfig.from_version(
+                version="2.8.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ETH_TEST_SESSIONS": PyTestConfig(
+                            markers="ethernet",
+                            run_test_stage_uid="ethernet_everest",
+                            stage_name="Ethernet Everest - FW. 2.8.0",
+                        )
+                    },
+                },
+            ),
         },
     ),
-    PartNumber.CAP_XCR_C: RackServiceConfigSpecifier.from_firmware(
+    PartNumber.CAP_XCR_C: RackServiceConfigSpecifier.from_version_configs(
         part_number=PartNumber.CAP_XCR_C,
         interface=Interface.ETH,
-        config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
-        version="2.4.0",
-        dictionary_type=DictionaryType.XDF_V2,
-        extra_data={
-            __EXECUTION_POLICY_KEY: "always",
-            __TEST_CONFIGS_KEY: {
-                "ETH_TEST_SESSIONS": PyTestConfig(
-                    markers="ethernet",
-                    run_test_stage_uid="ethernet_capitan",
-                    stage_name="Ethernet Capitan",
-                )
-            },
+        version_configs={
+            "2.4.0": VersionConfig.from_version(
+                version="2.4.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ETH_TEST_SESSIONS": PyTestConfig(
+                            markers="ethernet",
+                            run_test_stage_uid="ethernet_capitan",
+                            stage_name="Ethernet Capitan - FW. 2.4.0",
+                        )
+                    },
+                },
+            ),
+            "2.9.0": VersionConfig.from_version(
+                version="2.9.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "ETH_TEST_SESSIONS": PyTestConfig(
+                            markers="ethernet",
+                            run_test_stage_uid="ethernet_capitan",
+                            stage_name="Ethernet Capitan - FW. 2.9.0",
+                        )
+                    },
+                },
+            ),
         },
     ),
 })
 
 CAN_SETUP = SpecifierContainer({
-    PartNumber.EVE_XCR_C: RackServiceConfigSpecifier.from_firmware(
+    PartNumber.EVE_XCR_C: RackServiceConfigSpecifier.from_version_configs(
         part_number=PartNumber.EVE_XCR_C,
         interface=Interface.CAN,
-        config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
-        version="2.4.0",
-        dictionary_type=DictionaryType.XDF_V2,
-        extra_data={
-            __EXECUTION_POLICY_KEY: "always",
-            __TEST_CONFIGS_KEY: {
-                "CAN_TEST_SESSIONS": PyTestConfig(
-                    markers="canopen",
-                    run_test_stage_uid="canopen_everest",
-                    stage_name="CANopen Everest",
-                )
-            },
+        version_configs={
+            "2.4.0": VersionConfig.from_version(
+                version="2.4.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "CAN_TEST_SESSIONS": PyTestConfig(
+                            markers="canopen",
+                            run_test_stage_uid="canopen_everest",
+                            stage_name="CANopen Everest - FW. 2.4.0",
+                        )
+                    },
+                },
+            ),
+            "2.8.0": VersionConfig.from_version(
+                version="2.8.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/eve_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "CAN_TEST_SESSIONS": PyTestConfig(
+                            markers="canopen",
+                            run_test_stage_uid="canopen_everest",
+                            stage_name="CANopen Everest - FW. 2.8.0",
+                        )
+                    },
+                },
+            ),
         },
     ),
-    PartNumber.CAP_XCR_C: RackServiceConfigSpecifier.from_firmware(
+    PartNumber.CAP_XCR_C: RackServiceConfigSpecifier.from_version_configs(
         part_number=PartNumber.CAP_XCR_C,
         interface=Interface.CAN,
-        config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
-        version="2.4.0",
-        dictionary_type=DictionaryType.XDF_V2,
-        extra_data={
-            __EXECUTION_POLICY_KEY: "always",
-            __TEST_CONFIGS_KEY: {
-                "CAN_TEST_SESSIONS": PyTestConfig(
-                    markers="canopen",
-                    run_test_stage_uid="canopen_capitan",
-                    stage_name="CANopen Capitan",
-                )
-            },
+        version_configs={
+            "2.4.0": VersionConfig.from_version(
+                version="2.4.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "CAN_TEST_SESSIONS": PyTestConfig(
+                            markers="canopen",
+                            run_test_stage_uid="canopen_capitan",
+                            stage_name="CANopen Capitan - FW. 2.4.0",
+                        )
+                    },
+                },
+            ),
+            "2.9.0": VersionConfig.from_version(
+                version="2.9.0",
+                config_file=__CONFIG_FILES_PATH / "canopen/cap_xcr_c.xcf",
+                dictionary_type=DictionaryType.XDF_V2,
+                extra_data={
+                    __EXECUTION_POLICY_KEY: "always",
+                    __TEST_CONFIGS_KEY: {
+                        "CAN_TEST_SESSIONS": PyTestConfig(
+                            markers="canopen",
+                            run_test_stage_uid="canopen_capitan",
+                            stage_name="CANopen Capitan - FW. 2.9.0",
+                        )
+                    },
+                },
+            ),
         },
     ),
 })
@@ -192,7 +287,9 @@ ECAT_DEN_S_NET_E_SETUP = RackServiceConfigSpecifier.from_version_configs(
 ECAT_MULTISLAVE_SETUP = MultiRackServiceConfigSpecifier.create(
     identifier="ECAT_MULTISLAVE",
     specifiers=[
-        ECAT_SETUP.get_specifier_by_identifier(PartNumber.EVE_XCR_E),
+        ECAT_SETUP.get_specifier_by_identifier_with_version(
+            identifier=PartNumber.EVE_XCR_E, version="2.6.0"
+        ),
         ECAT_SETUP.get_specifier_by_identifier_with_version(
             identifier=PartNumber.CAP_XCR_E, version="2.6.0"
         ),
