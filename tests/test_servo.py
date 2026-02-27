96063fb12403009d42ee3a24c00792164cc63efd
julieta.prieto <julieta.prieto@novanta.com>
2026-02-27 08:26:27 +0100

    INGK-1250 fix tests for cap-xcr-c

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -269,6 +269,18 @@ def test_load_configuration_to_subnode_zero(setup_descriptor, servo, tmp_path) -
 @pytest.mark.valid_versions_for_product(part_number="EVE-NET-E", max="2.8.9")
 @pytest.mark.valid_versions_for_product(part_number="EVE-XCR-E", max="2.8.9")
 @pytest.mark.valid_versions_for_product(part_number="DEN-NET-E", max="2.8.9")
+@pytest.mark.valid_versions_for_product(
+    part_number="CAP-XCR-C", max="2.8.9", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="CAP-NET-C", max="2.8.9", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="EVE-XCR-C", max="2.8.9", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="EVE-NET-C", max="2.8.9", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
 def test_store_parameters_timed_recovery(servo, environment: "Environment", mocker) -> None:
     user_over_voltage_register = "DRV_PROT_USER_OVER_VOLT"
 
@@ -308,6 +320,18 @@ def test_store_parameters_timed_recovery(servo, environment: "Environment", mock
 @pytest.mark.valid_versions_for_product(part_number="EVE-NET-E", min="2.9.0")
 @pytest.mark.valid_versions_for_product(part_number="EVE-XCR-E", min="2.9.0")
 @pytest.mark.valid_versions_for_product(part_number="DEN-NET-E", min="2.9.0")
+@pytest.mark.valid_versions_for_product(
+    part_number="CAP-XCR-C", min="2.9.0", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="CAP-NET-C", min="2.9.0", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="EVE-XCR-C", min="2.9.0", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
+@pytest.mark.valid_versions_for_product(
+    part_number="EVE-NET-C", min="2.9.0", interfaces=[Interface.CAN, Interface.ETHERNET]
+)
 def test_store_parameters_polling_status(servo, environment: "Environment", mocker) -> None:
     # Both registers exist in this dictionary
     servo.dictionary.get_register("DRV_STORE_STATUS_MOCO")
