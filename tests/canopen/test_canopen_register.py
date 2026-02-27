fbcacde6389d43ee5513b7988c8ceaa40bbff6dd
julieta.prieto <julieta.prieto@novanta.com>
2026-02-27 12:57:23 +0100

    INGK-1250 update fw version requirement in canopen register test

--- tests/canopen/test_canopen_register.py
+++ tests/canopen/test_canopen_register.py
@@ -107,7 +107,7 @@ def _canopen_connection_register_assertions(
 
 
 @pytest.mark.canopen
-@pytest.mark.valid_versions_for_standard_comocos(max="2.8.9")
+@pytest.mark.valid_versions_for_standard_comocos(max="2.7.9")
 def test_canopen_connection_register_old_firmware(
     servo: "CanopenServo", net: "CanopenNetwork"
 ) -> None:
@@ -117,7 +117,7 @@ def test_canopen_connection_register_old_firmware(
 
 
 @pytest.mark.canopen
-@pytest.mark.valid_versions_for_standard_comocos(min="2.9.0")
+@pytest.mark.valid_versions_for_standard_comocos(min="2.8.0")
 def test_canopen_connection_register(servo: "CanopenServo", net: "CanopenNetwork") -> None:
     """New firmware versions represent None units with None, not '-'."""
     register = _canopen_connection_register_assertions(servo=servo, net=net)
