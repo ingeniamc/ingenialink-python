f7f743d6034767d96306f86e5468a71a5ba083da
MartinALL91 <martinacostallano@gmail.com>
2026-02-27 12:19:38 +0100

    Update setup name

--- tests/setups/virtual_drive_specifier.py
+++ tests/setups/virtual_drive_specifier.py
@@ -1,5 +1,3 @@
 from summit_testing_framework.setups.specifiers import VirtualDriveSpecifier
 
-VIRTUAL_DRIVE_SETUP = VirtualDriveSpecifier.from_dictionary(
-    ip="127.0.0.1", port=1061, extra_data={"execution_policy": "always"}
-)
+VIRTUAL_DRIVE_ETHERNET_SETUP = VirtualDriveSpecifier.from_ethernet(extra_data={"execution_policy": "always"})
