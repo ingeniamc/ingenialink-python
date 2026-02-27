35220228eb1b3169d5f2e2fb0ccb68ec46bc0212
MartinALL91 <martinacostallano@gmail.com>
2026-02-27 12:45:12 +0100

    CIT-585 Fix ruff

--- tests/setups/virtual_drive_specifier.py
+++ tests/setups/virtual_drive_specifier.py
@@ -1,3 +1,5 @@
 from summit_testing_framework.setups.specifiers import VirtualDriveSpecifier
 
-VIRTUAL_DRIVE_ETHERNET_SETUP = VirtualDriveSpecifier.from_ethernet(extra_data={"execution_policy": "always"})
+VIRTUAL_DRIVE_ETHERNET_SETUP = VirtualDriveSpecifier.from_ethernet(
+    extra_data={"execution_policy": "always"}
+)
