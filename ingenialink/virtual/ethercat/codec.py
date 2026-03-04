dd3d819c8fced49643a22037ff568ff68749b90c
MartinALL91 <martinacostallano@gmail.com>
2026-03-04 16:31:55 +0100

    SVD-35 Remove old codec file

--- ingenialink/virtual/ethercat/codec.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible re-export of the shared SDO codec."""
-
-from ingenialink.virtual.codec import deserialize_sdo_frame, serialize_sdo_frame
-
-__all__ = ["deserialize_sdo_frame", "serialize_sdo_frame"]
