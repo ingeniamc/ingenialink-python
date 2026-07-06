d2294dbf5267ca78d1ef98f4d196b97ca449855c
julieta.prieto <julieta.prieto@novanta.com>
2026-07-06 13:25:32 +0200

    CIT-713 update to new config files

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -1,9 +1,5 @@
-from pathlib import Path
-
+import summit_drives_ci_configs.config_files as config_files
 from summit_testing_framework.jenkins.pytest_config import PyTestConfig
-from summit_testing_framework.pytest_helpers.import_helpers import (
-    import_module_from_local_path,
-)
 from summit_testing_framework.setups.specifier_container import SpecifierContainer
 from summit_testing_framework.setups.specifiers import (
     DictionaryType,
@@ -15,14 +11,6 @@ from summit_testing_framework.setups.specifiers import (
 
 from ingenialink.dictionary import Interface
 
-# This file is used to export the specifiers to a JSON. Since tests is not a package,
-# we need to import the config files using a helper function to be able to access them
-_config_files = import_module_from_local_path(
-    module_name="config_files", module_path=Path(__file__).parent / "config_files"
-)
-assert _config_files is not None
-
-
 __EXECUTION_POLICY_KEY: str = "execution_policy"
 __TEST_CONFIGS_KEY: str = "test_configs"
 
@@ -33,7 +21,7 @@ ECAT_SETUP = SpecifierContainer({
         version_configs={
             "2.6.0": VersionConfig.from_version(
                 version="2.6.0",
-                config_file=_config_files.EVE_XCR_E_CONFIG,
+                config_file=config_files.EVE_XCR_E_2_1_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -48,7 +36,7 @@ ECAT_SETUP = SpecifierContainer({
             ),
             "2.8.1": VersionConfig.from_version(
                 version="2.8.1",
-                config_file=_config_files.EVE_XCR_E_CONFIG,
+                config_file=config_files.EVE_XCR_E_2_1_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
@@ -69,7 +57,7 @@ ECAT_SETUP = SpecifierContainer({
         version_configs={
             "2.6.0": VersionConfig.from_version(
                 version="2.6.0",
-                config_file=_config_files.CAP_XCR_E_CONFIG,
+                config_file=config_files.CAP_XCR_E_2_2_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -84,7 +72,7 @@ ECAT_SETUP = SpecifierContainer({
             ),
             "2.10.0": VersionConfig.from_version(
                 version="2.10.0",
-                config_file=_config_files.CAP_XCR_E_CONFIG,
+                config_file=config_files.CAP_XCR_E_2_9_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V3,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
@@ -108,7 +96,7 @@ ETH_SETUP = SpecifierContainer({
         version_configs={
             "2.4.0": VersionConfig.from_version(
                 version="2.4.0",
-                config_file=_config_files.EVE_XCR_C_CONFIG,
+                config_file=config_files.EVE_XCR_C_2_1_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -123,7 +111,7 @@ ETH_SETUP = SpecifierContainer({
             ),
             "2.8.1": VersionConfig.from_version(
                 version="2.8.1",
-                config_file=_config_files.EVE_XCR_C_CONFIG,
+                config_file=config_files.EVE_XCR_C_2_8_1_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
@@ -144,7 +132,7 @@ ETH_SETUP = SpecifierContainer({
         version_configs={
             "2.4.0": VersionConfig.from_version(
                 version="2.4.0",
-                config_file=_config_files.CAP_XCR_C_CONFIG,
+                config_file=config_files.CAP_XCR_C_2_2_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -159,7 +147,7 @@ ETH_SETUP = SpecifierContainer({
             ),
             "2.10.0": VersionConfig.from_version(
                 version="2.10.0",
-                config_file=_config_files.CAP_XCR_C_CONFIG,
+                config_file=config_files.CAP_XCR_C_2_2_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V3,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
@@ -183,7 +171,7 @@ CAN_SETUP = SpecifierContainer({
         version_configs={
             "2.4.0": VersionConfig.from_version(
                 version="2.4.0",
-                config_file=_config_files.EVE_XCR_C_CONFIG,
+                config_file=config_files.EVE_XCR_C_2_1_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -198,7 +186,7 @@ CAN_SETUP = SpecifierContainer({
             ),
             "2.8.1": VersionConfig.from_version(
                 version="2.8.1",
-                config_file=_config_files.EVE_XCR_C_CONFIG,
+                config_file=config_files.EVE_XCR_C_2_8_1_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
@@ -219,7 +207,7 @@ CAN_SETUP = SpecifierContainer({
         version_configs={
             "2.4.0": VersionConfig.from_version(
                 version="2.4.0",
-                config_file=_config_files.CAP_XCR_C_CONFIG,
+                config_file=config_files.CAP_XCR_C_2_2_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V2,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "weekends",
@@ -234,7 +222,7 @@ CAN_SETUP = SpecifierContainer({
             ),
             "2.10.0": VersionConfig.from_version(
                 version="2.10.0",
-                config_file=_config_files.CAP_XCR_C_CONFIG,
+                config_file=config_files.CAP_XCR_C_2_2_0_CONFIG,
                 dictionary_type=DictionaryType.XDF_V3,
                 extra_data={
                     __EXECUTION_POLICY_KEY: "always",
