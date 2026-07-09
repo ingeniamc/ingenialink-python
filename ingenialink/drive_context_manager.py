3879d31dc92e6aafaa614ccbfc852d1c3a145dbc
Pol Feliu Cuberes <>
2026-07-09 09:41:51 +0200

    CIT-740 Clarify comments on PDO mapping registers restoration in DriveContextManager

--- ingenialink/drive_context_manager.py
+++ ingenialink/drive_context_manager.py
@@ -663,6 +663,9 @@ class DriveContextManager:
                 # CIT-751 PDO related registers that are changed by the drive
                 "ETG_COMMS_SM_OUTPUT_SYNC_TYPE",
                 "ETG_COMMS_SM_INPUT_SYNC_TYPE",
+                # Canopen PDO mapping registers are not implemented and are not restorable individually.
+                "CIA301_COMMS_RPDO*",
+                "CIA301_COMMS_TPDO*",
             )
         )
 
