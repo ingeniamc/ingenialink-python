7aa44ada57108eaa6128f1ae99ecdde00b8bda02
Pol Feliu Cuberes <>
2026-07-24 12:56:59 +0200

    CIT-739 replace dictionaries commited on the repo by att fetched

---
+++ tests/dictionaries/fixtures.py
@@ -0,0 +1,18 @@
+from collections.abc import Callable
+from pathlib import Path
+from typing import Optional
+
+import pytest
+from summit_testing_framework.setups.specifiers import DictionaryType, DictionaryVersion
+
+from ingenialink.dictionary import Interface
+
+
+@pytest.fixture(scope="session")
+def den_net_e_2_8_0_xdf_v3(
+    product_dictionary: Callable[[str, DictionaryVersion, Optional[Interface]], Path],
+) -> Path:
+    return product_dictionary(
+        "DEN-NET-E",
+        DictionaryVersion("2.8.0", DictionaryType.XDF_V3),
+    )
