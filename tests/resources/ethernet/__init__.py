ba37c1269cc1ece97abd864acba12c95d9509654
MartinALL91 <martinacostallano@gmail.com>
2026-08-04 14:59:30 +0200

    INGK-1279 Update test

--- tests/resources/ethernet/__init__.py
+++ tests/resources/ethernet/__init__.py
@@ -4,3 +4,4 @@ resources_path = absolute_module_path = Path(__file__).parent
 
 TEST_DICT_ETHERNET = (resources_path / "test_dict_eth.xdf").as_posix()
 TEST_DICT_ETHERNET_AXIS = (resources_path / "test_dict_eth_axis.xdf").as_posix()
+TEST_DICT_SDCP_v3 = (resources_path / "sdcp_example.xdf3").as_posix()
