bd4b01b628bb6f4e0ce75f60d0c690cbc4c113d1
Pol Feliu Cuberes <>
2026-08-06 13:48:10 +0200

    Fix import formatting

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import summit_drives_ci_configs.config_files as config_files
 from summit_testing_framework.jenkins.pytest_config import PyTestConfig
 from summit_testing_framework.setups.specifier_container import SpecifierContainer
 from summit_testing_framework.setups.specifiers import (
@@ -11,6 +10,7 @@ from summit_testing_framework.setups.specifiers import (
     VersionConfig,
 )
 
+import summit_drives_ci_configs.config_files as config_files
 from ingenialink.dictionary import Interface
 
 __EXECUTION_POLICY_KEY: str = "execution_policy"
