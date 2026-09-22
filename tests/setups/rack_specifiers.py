c7645f948cfef16076ef2668be17700e6b926445
Pol Feliu Cuberes <>
2026-09-22 17:56:36 +0200

    MOT3-5670 Fix CI import ordering

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -1,6 +1,3 @@
-import summit_drives_ci_configs.config_files as config_files
-from ingenialink.dictionary import Interface
-
 from summit_testing_framework.jenkins.pytest_config import PyTestConfig
 from summit_testing_framework.setups.specifier_container import SpecifierContainer
 from summit_testing_framework.setups.specifiers import (
@@ -10,6 +7,9 @@ from summit_testing_framework.setups.specifiers import (
     RackServiceConfigSpecifier,
     VersionConfig,
 )
+
+import summit_drives_ci_configs.config_files as config_files
+from ingenialink.dictionary import Interface
 
 __EXECUTION_POLICY_KEY: str = "execution_policy"
 __TEST_CONFIGS_KEY: str = "test_configs"
